0c1ff0b4dae68c0394f83c95a67465c52da6493d
Raymond Burkholder <raymond@burkholder.net>
2008-08-26 13:40:23 +0000

    Implementation of TimeSource.
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@253 43143c16-083f-4785-a0a0-245a1fb241a9

--- LibCommon/TimeSource.cpp
+++ LibCommon/TimeSource.cpp
@@ -1,10 +1,11 @@
 #include "StdAfx.h"
 #include "TimeSource.h"
 
-CTimeSource::CTimeSource(void)
-{
+bool CTimeSource::m_bInSimulation = false;
+ptime CTimeSource::m_dtSimulationTime = boost::date_time::not_a_date_time;
+
+CTimeSource::CTimeSource(void) {
 }
 
-CTimeSource::~CTimeSource(void)
-{
+CTimeSource::~CTimeSource(void) {
 }
--- LibCommon/TimeSource.h
+++ LibCommon/TimeSource.h
@@ -1,11 +1,26 @@
 #pragma once
 
-class CTimeSource {
+#include "boost/date_time/posix_time/posix_time.hpp"
+using namespace boost::posix_time;
+using namespace boost::gregorian;
+
+class CTimeSource : boost::noncopyable {
 public:
-  CTimeSource(void);
   ~CTimeSource(void);
-  static void Internal( void ) {};
-  static void External( void ) {};
+  static ptime External( void ) { return boost::posix_time::microsec_clock::local_time(); };
+  static ptime Internal( void ) { 
+    if ( m_bInSimulation ) 
+      return m_dtSimulationTime;
+    else 
+      return External(); 
+  };
+  static void SetSimulationMode( void ) { m_bInSimulation = true; m_dtSimulationTime = boost::date_time::not_a_date_time; };
+  static void ResetSimulationMode( void ) { m_bInSimulation = false; };
+  static bool GetSimulationMode( void ) { return m_bInSimulation; };
+  static void SetSimulationTime( const ptime &dt ) { m_dtSimulationTime = dt; };
 protected:
+  static bool m_bInSimulation;
+  static ptime m_dtSimulationTime;
 private:
+  CTimeSource(void);
 };
