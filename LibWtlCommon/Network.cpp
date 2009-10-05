50e329e94f4b56b02bbc5b90234b0467b60d9aea
Raymond Burkholder <raymond@burkholder.net>
2009-10-05 02:36:08 +0000

    Made progress on CNetwork
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@316 43143c16-083f-4785-a0a0-245a1fb241a9

--- LibWtlCommon/Network.cpp
+++ LibWtlCommon/Network.cpp
@@ -14,12 +14,16 @@
 #include "StdAfx.h"
 #include "Network.h"
 
+#include <boost/bind.hpp>
+
 using boost::asio::ip::tcp;
 
 CNetwork::CNetwork(CAppModule* pModule, const structMessages& messages)
-: CGuiThreadImpl<CNetwork>( pModule ), m_Messages( messages )
+: CGuiThreadImpl<CNetwork>( pModule ), m_Messages( messages ),
+  m_timer( m_io, boost::posix_time::seconds( 1 ) ), m_bKeepTimerActive( true ),
+  m_psocket( new tcp::socket( m_io ) ), 
+  m_bSocketOpen( false ) //, m_bSocketOpened( false )
 {
-  m_psocket = new tcp::socket( m_io );
 }
 
 CNetwork::~CNetwork(void) {
@@ -27,52 +31,105 @@ CNetwork::~CNetwork(void) {
 
 BOOL CNetwork::InitializeThread( void ) {
 
-// synchronous
-//  socket.connect(server_endpoint);
-//boost::system::error_code ec;
-//socket.connect(server_endpoint, ec);
+  m_asioThread = boost::thread( &CNetwork::AsioThread, this );
 
-// async
-// void your_completion_handler(const boost::system::error_code& ec);
-//  socket.async_connect(server_endpoint, your_completion_handler);
-  // io_service::run()
+  BOOL b = PostMessage( m_Messages.msgInitialized );
 
-
-  BOOL b;
-  if ( 0 != m_Messages.msgInitialized ) {
-    if ( 0 != m_Messages.hWnd ) {
-      b = ::PostMessage( m_Messages.hWnd, m_Messages.msgInitialized, NULL, NULL );
-    }
-    
-  }
   return TRUE;
+}
+
+void CNetwork::AsioThread( void ) {
+
+  m_io.run();  // handles async timer and async socket 
+#ifdef _DEBUG
+  OutputDebugString( "ASIO thread exited.\n" );
+#endif
 }
 
 void CNetwork::CleanupThread( DWORD dw ) {
 
-  BOOL b;
-  if ( 0 != m_Messages.msgClosed ) {
-    if ( 0 != m_Messages.hWnd ) {
-      b = ::PostMessage( m_Messages.hWnd, m_Messages.msgClosed, NULL, NULL );
-    }
-    
+  // http://www.boost.org/doc/libs/1_40_0/doc/html/boost_asio/reference/deadline_timer.html
+
+  m_bKeepTimerActive = false;
+  m_timer.cancel();
+
+  if ( m_bSocketOpen ) {
+    m_psocket->close();
+    m_bSocketOpen = false;
+  }
+
+  m_asioThread.join();  // wait for i/o thread to terminate
+
+  BOOL b = PostMessage( m_Messages.msgClosed );
+}
+
+LRESULT CNetwork::OnConnect( UINT, WPARAM wParam, LPARAM, BOOL &bHandled ) {
+
+  const structConnection& connection  = reinterpret_cast<const structConnection&>( wParam ); 
+
+  // http://www.boost.org/doc/libs/1_40_0/doc/html/boost_asio/reference/basic_stream_socket/async_connect.html
+  tcp::endpoint endpoint( boost::asio::ip::address::from_string( connection.sAddress ), connection.nPort );
+  m_psocket->async_connect( endpoint, boost::bind<void>( &CNetwork::ConnectHandler, this, boost::asio::placeholders::error ) );
+
+  bHandled = true;
+  return 1;
+}
+
+void CNetwork::ConnectHandler( const boost::system::error_code& error ) {
+  if ( error ) {
+    BOOL b = PostMessage( m_Messages.msgError, ERROR_CONNECT );
+  }
+  else {
+    m_bSocketOpen = true;
+    BOOL b = PostMessage( m_Messages.msgConnected );
   }
 }
 
-LRESULT CNetwork::OnOpen( UINT, WPARAM wParam, LPARAM, BOOL &bHandled ) {
+void CNetwork::TimerHandler( const boost::system::error_code& error ) {
+  if ( m_bKeepTimerActive ) {
+    m_timer.expires_at( m_timer.expires_at() + boost::posix_time::seconds( 1 ) );
+    m_timer.async_wait( boost::bind( &CNetwork::TimerHandler, this, boost::asio::placeholders::error ) );
+  }
+}
 
-  const structConnection& pConn = reinterpret_cast<const structConnection&>( wParam ); 
+LRESULT CNetwork::OnSend( UINT, WPARAM wParam, LPARAM, BOOL &bHandled ) {
+
+  buffer_t* pbuffer = reinterpret_cast<buffer_t*>( wParam );
+
+  if ( m_bSocketOpen ) {
+    boost::asio::async_write( *m_psocket, boost::asio::buffer( *pbuffer ), 
+      boost::bind( &CNetwork::WriteHandler, this, 
+      boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred, pbuffer )
+      );
+  }
+  else {
+    BOOL b = PostMessage( m_Messages.msgError, ERROR_SOCKET );
+  }
 
   bHandled = true;
   return 1;
 }
 
-LRESULT CNetwork::OnSend( UINT, WPARAM, LPARAM, BOOL &bHandled ) {
-  bHandled = true;
-  return 1;
+void CNetwork::WriteHandler( const boost::system::error_code& error, std::size_t bytes_transferred, buffer_t* pbuffer ) {
+  if ( error ) {
+    BOOL b = PostMessage( m_Messages.msgError, ERROR_WRITE ); 
+  }
+  if ( 0 != m_Messages.msgSendDone ) {
+    if ( 0 != m_Messages.hWnd ) {
+      BOOL b = ::PostMessage( m_Messages.hWnd, m_Messages.msgSendDone, reinterpret_cast<WPARAM>( pbuffer ), NULL );
+    }
+  }
+
 }
 
-LRESULT CNetwork::OnClose( UINT, WPARAM, LPARAM, BOOL &bHandled ) {
+LRESULT CNetwork::OnDisconnect( UINT, WPARAM, LPARAM, BOOL &bHandled ) {
+
+  //m_psocket->cancel();  //  boost::asio::error::operation_aborted, boost::asio::error::operation_not_supported on xp
+  m_psocket->close();
+  m_bSocketOpen = false;
+
+  BOOL b = PostMessage( m_Messages.msgDisconnected );
+
   bHandled = true;
   return 1;
 }
@@ -80,4 +137,14 @@ LRESULT CNetwork::OnClose( UINT, WPARAM, LPARAM, BOOL &bHandled ) {
 LRESULT CNetwork::OnProcessed( UINT, WPARAM, LPARAM, BOOL &bHandled ) {
   bHandled = true;
   return 1;
+}
+
+BOOL CNetwork::PostMessage( UINT msg, WPARAM wParam, LPARAM lParam ) {
+  BOOL b = false;
+  if ( 0 != msg ) {
+    if ( 0 != m_Messages.hWnd ) {
+      b = ::PostMessage( m_Messages.hWnd, msg, wParam, lParam );
+    }
+  }
+  return b;
 }
--- LibWtlCommon/Network.h
+++ LibWtlCommon/Network.h
@@ -14,36 +14,59 @@
 #pragma once
 
 #include <codeproject/thread.h>  // class inbound messages
+
 #include <boost/asio.hpp>  // class outbound processing
-#include <boost/array.hpp>
+#include <boost/thread.hpp>  // separate thread for asio run processing
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #include <string>
+#include <vector>
+
+#include "LibCommon/ReusableBuffers.h"
 
 
 // use asio post messages to send commands 
 // use CThread PostMessages to return responses
 // use circular buffer of 20 messages
 // post message when circular buffer is empty
-// based upon Lock-Free Queues, http://www.ddj.com/hpc-high-performance-computing/208801974
+// based upon Lock-Free Queues? http://www.ddj.com/hpc-high-performance-computing/208801974
 // input streams are cr/lf delimited
 // LOG received buffer length to see if we are actually using big buffers on reception
 // constructor uses static init structure, rather than dynamic update one message types
 // maybe do dynamic message pointer updates later
+// three threads:
+//   * external thread for routines that send message to this class
+//   * asio thread for processing outbound and inbound
+//   * windows thread for processing windows messages
+// when running with socket async io, is a timer needed to keep asio running?
 
 class CNetwork: public CGuiThreadImpl<CNetwork> {
 public:
 
-  enum enumMessageTypes {  // messages accepted by thread
-    WM_NETWORK_OPEN = WM_USER + 1,  // open the port
+  // message ids for messages delivered to this class from external caller
+  enum enumMessageTypes { 
+    WM_NETWORK_CONNECT = WM_USER + 1,  // open the port
+    WM_NETWORK_DISCONNECT,  // close the port
     WM_NETWORK_SEND,  // send string over network port
-    WM_NETWORK_CLOSE,  // close the port
-    WM_NETWORK_PROCESSED // message has been processed, update buffer queue with processed buffer
+    WM_NETWORK_PROCESSED // message has been processed, return buffer to queue for re-use
   };
 
+  enum enumMessageErrorTypes {
+    OK = 0,
+    ERROR_WRITE,
+    ERROR_SOCKET,
+    ERROR_CONNECT
+  };
+
+  // pre-initialized message ids for messages delivered to and accepted by external caller
   struct structMessages {  // information needed by port processing thread
     HWND hWnd;        // handle of window to which message is sent
-    UINT msgProcess;  // message id when sending line to process
-    UINT msgInitialized;   // message id when socket system has been opened and initialized
-    UINT msgClosed;   // message id when socket system has been closed
+    UINT msgInitialized;   // message id when class thread has been  initialized
+    UINT msgClosed;   // message id when class thread has ended
+    UINT msgConnected;    // message id when socket has been connected
+    UINT msgDisconnected;  // message id when socket has been disconnected/closed
+    UINT msgSendDone; // returns buffer used by the send message
+    UINT msgProcess;  // message id when sending line of text to process
     UINT msgError;    // message id when error is encountered
     structMessages(void): hWnd( 0 ), msgProcess( 0 ), msgInitialized( 0 ), msgClosed( 0 ), msgError( 0 ) {};
     structMessages(HWND hWnd_, UINT msgProcess_, UINT msgInitialized_, UINT msgClosed_, UINT msgError_ )
@@ -55,7 +78,9 @@ public:
     unsigned short nPort;
   };
 
-  struct
+  typedef unsigned char bufferelement_t;
+  typedef std::vector<bufferelement_t> buffer_t;
+  typedef CBufferRepository<buffer_t> repository_t;
 
   CNetwork(CAppModule* pModule, const structMessages&);
   ~CNetwork(void);
@@ -63,29 +88,45 @@ public:
 protected:
   BEGIN_MSG_MAP_EX(CNetwork)
 //    MSG_WM_TIMER(OnTimer)
-    MESSAGE_HANDLER(WM_NETWORK_OPEN, OnOpen)
+    MESSAGE_HANDLER(WM_NETWORK_CONNECT, OnConnect)
     MESSAGE_HANDLER(WM_NETWORK_SEND, OnSend)
     MESSAGE_HANDLER(WM_NETWORK_PROCESSED, OnProcessed)
-    MESSAGE_HANDLER(WM_NETWORK_CLOSE, OnClose)
+    MESSAGE_HANDLER(WM_NETWORK_DISCONNECT, OnDisconnect)
   END_MSG_MAP()
+
+  LRESULT OnConnect( UINT, WPARAM, LPARAM, BOOL &bHandled);
+  LRESULT OnSend( UINT, WPARAM, LPARAM, BOOL &bHandled);
+  LRESULT OnProcessed( UINT, WPARAM, LPARAM, BOOL &bHandled);
+  LRESULT OnDisconnect( UINT, WPARAM, LPARAM, BOOL &bHandled);
 
   BOOL InitializeThread( void );
   void CleanupThread( DWORD );
 
-  LRESULT OnOpen( UINT, WPARAM, LPARAM, BOOL &bHandled);
-  LRESULT OnSend( UINT, WPARAM, LPARAM, BOOL &bHandled);
-  LRESULT OnProcessed( UINT, WPARAM, LPARAM, BOOL &bHandled);
-  LRESULT OnClose( UINT, WPARAM, LPARAM, BOOL &bHandled);
-
-  boost::asio::io_service m_io;
-  boost::asio::ip::tcp::socket* m_psocket;
-
+  BOOL PostMessage( UINT, WPARAM = NULL, LPARAM = NULL );
 
 private:
 #define NETWORK_INBOUND_BUF_SIZE 5120
 
   structMessages m_Messages;
 
-  boost::array<char, NETWORK_INBOUND_BUF_SIZE> m_buf;
+  bool m_bKeepTimerActive;
+//  bool m_bSocketOpened;
+  bool m_bSocketOpen;
+
+  boost::thread m_asioThread;
+
+  boost::asio::io_service m_io;
+  boost::asio::ip::tcp::socket* m_psocket;
+
+  boost::asio::deadline_timer m_timer;  // used to keep asio.run something to keep busy with
+
+  //boost::array<char, NETWORK_INBOUND_BUF_SIZE> m_buf;
+  repository_t m_repository;
+
+  void ConnectHandler( const boost::system::error_code& error );
+  void TimerHandler( const boost::system::error_code& error );
+  void WriteHandler( const boost::system::error_code& error, std::size_t bytes_transferred, buffer_t* );
+
+  void AsioThread( void );
 
 };
