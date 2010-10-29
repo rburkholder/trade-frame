42b3e795680bef74d1a079a73acfdd40abb364d9
Raymond Burkholder <raymond@burkholder.net>
2010-10-29 14:12:46 +0000

    Adding new files to project.  Handles session management with sqlite.
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@497 43143c16-083f-4785-a0a0-245a1fb241a9

---
+++ LibSqlite/DbSession.cpp
@@ -0,0 +1,84 @@
+/************************************************************************
+ * Copyright(c) 2010, One Unified. All rights reserved.                 *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+#include "StdAfx.h"
+
+#include <stdexcept>
+#include <cassert>
+
+#include "DbSession.h"
+
+
+void PrepareStatement( 
+  const std::string& sErrPrefix, const std::string& sSqlOp, 
+  sqlite3* pDb, sqlite3_stmt** pStmt ) {
+
+  int rtn;
+
+  if ( NULL == *pStmt ) {
+    rtn = sqlite3_prepare_v2( pDb, sSqlOp.c_str(), -1, pStmt, NULL );
+    if ( SQLITE_OK != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ":  error in prepare";
+      throw std::runtime_error( sErr );
+    }
+  }
+  else {
+    rtn = sqlite3_reset( *pStmt );
+    if ( SQLITE_OK != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ":  error in reset";
+      throw std::runtime_error( sErr );
+    }
+  }
+
+}
+
+
+CDbSession::CDbSession(void)
+: m_db( 0 ), m_bDbOpened( false )
+{
+}
+
+
+CDbSession::CDbSession( const char* szDbFileName )
+: m_db( 0 ), m_bDbOpened( false )
+{
+  Open( szDbFileName );
+}
+
+
+CDbSession::~CDbSession(void)
+{
+  Close();
+}
+
+void CDbSession::Open( const char* szDbFileName ) {
+  int rtn = sqlite3_open_v2( szDbFileName, &m_db, SQLITE_OPEN_READWRITE, 0 );
+  if ( SQLITE_OK != rtn ) {
+    m_db = 0;
+    throw std::runtime_error( "Db open error" );
+  }
+
+  m_sDbFileName = szDbFileName;
+  m_bDbOpened = true;
+}
+
+void CDbSession::Close( void ) {
+  if ( m_bDbOpened ) {
+    int rtn = sqlite3_close( m_db );
+    m_db = 0;
+    m_bDbOpened = false;
+    assert( SQLITE_OK == rtn );
+  }
+}
---
+++ LibSqlite/DbSession.h
@@ -0,0 +1,168 @@
+/************************************************************************
+ * Copyright(c) 2010, One Unified. All rights reserved.                 *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+#pragma once
+
+#include <string>
+
+#include "sqlite3.h"
+
+template<typename A, typename T> // A=Action, T=Type
+void Field( A& a, const std::string& sName, T& var, const std::string& sDbType ) {
+  a.registerField( sName, var, sDbType );
+}
+
+template<typename K, typename M, typename P, typename F> 
+void LoadObject( 
+  const std::string& sErrPrefix, const std::string& sSqlSelect, 
+  K& key, 
+  sqlite3* pDb,
+  sqlite3_stmt** pStmt, 
+  M& map,  // map
+  P& p, // shared ptr
+  F fConstructInstance  // function to construct instance of class
+  ) 
+  {
+
+  M::iterator iter = map.find( key );
+  if ( map.end() != iter ) {
+    p = iter->second;
+  }
+  else {
+    if ( NULL == pDb ) {
+      std::string sErr( sErrPrefix );
+      sErr += ":  no record available";
+      throw std::runtime_error( sErr );
+    }
+    else {
+      int rtn;
+
+      PrepareStatement( sErrPrefix, sSqlSelect, pDb, pStmt );
+    
+      rtn = p->BindDbKey( *pStmt );
+      if ( SQLITE_OK != rtn ) {
+        std::string sErr( sErrPrefix );
+        sErr += ":  error in bind";
+        throw std::runtime_error( sErr );
+      }
+      rtn = sqlite3_step( *pStmt );
+      if ( SQLITE_ROW == rtn ) {
+        p.reset( fConstructInstance( key, *pStmt ) );
+      }
+      else {
+        std::string sErr( sErrPrefix );
+        sErr += ":  error in load";
+        throw std::runtime_error( sErr );
+      }
+
+      map.insert( std::pair<K,P>( key, p ) );
+    }
+  }
+}
+
+template<typename K, typename P>
+void SqlOpOnObject( 
+  const std::string& sErrPrefix, const std::string& sSqlOp, 
+  sqlite3* pDb,
+  sqlite3_stmt** pStmt, 
+  K& key, 
+  P& p
+  )
+{
+  if ( NULL != pDb ) {
+    int rtn;
+
+    PrepareStatement( sErrPrefix, sSqlOp, pDb, pStmt );
+
+    rtn = p->BindDbKey( *pStmt );
+    if ( SQLITE_OK != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ": error in BindDbKey";
+      throw std::runtime_error( sErr );
+    }
+    rtn = p->BindDbVariables( *pStmt );
+    if ( SQLITE_OK != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ": error in BindDbVariables";
+      throw std::runtime_error( sErr );
+    }
+    rtn = sqlite3_step( *pStmt );
+    if ( SQLITE_DONE != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ": error in step";
+      throw std::runtime_error( sErr );
+    }
+  }
+
+}
+
+template<typename K, typename P>
+void DeleteObject( 
+  const std::string& sErrPrefix, const std::string& sSqlOp, 
+  sqlite3* pDb,
+  sqlite3_stmt** pStmt, 
+  K& key, 
+  P& p
+  )
+{
+  if ( NULL != pDb ) {
+
+    int rtn;
+
+    PrepareStatement( sErrPrefix, sSqlOp, pDb, pStmt );
+
+    rtn = p->BindDbKey( *pStmt );
+    if ( SQLITE_OK != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ": error in BindDbKey";
+      throw std::runtime_error( sErr );
+    }
+
+    rtn = sqlite3_step( *pStmt );
+    if ( SQLITE_DONE != rtn ) {
+      std::string sErr( sErrPrefix );
+      sErr += ": error in step";
+      throw std::runtime_error( sErr );
+    }
+
+  }
+
+}
+
+void PrepareStatement( 
+  const std::string& sErrPrefix, const std::string& sSqlOp, 
+  sqlite3* pDb, sqlite3_stmt** pStmt );
+
+
+
+class CDbSession {
+public:
+  CDbSession(void);
+  CDbSession( const char* szDbFileName );
+  virtual ~CDbSession(void);
+
+  void Open( const char* szDbFileName );
+  void Close( void );
+
+  sqlite3* GetDb( void ) { return m_db; };
+
+protected:
+private:
+
+  bool m_bDbOpened;
+
+  sqlite3* m_db;
+
+  std::string m_sDbFileName;
+};
+
