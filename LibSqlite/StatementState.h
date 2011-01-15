b781d7eba3f1a2bf25725d89f39702304b77c5fc
Raymond Burkholder <raymond@burkholder.net>
2011-01-15 20:25:01 +0000

    Adding some missing files.
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@530 43143c16-083f-4785-a0a0-245a1fb241a9

---
+++ LibSqlite/StatementState.h
@@ -0,0 +1,30 @@
+/************************************************************************
+ * Copyright(c) 2010, One Unified. All rights reserved.                 *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+#pragma once
+
+#include "sqlite3.h"
+
+namespace ou {
+namespace db {
+namespace sqlite {
+
+  struct structStatementState {
+  sqlite3_stmt* pStmt;
+  bool bIsReset;  // ensure it is reset just prior to execute
+  structStatementState( void ) : pStmt( 0 ), bIsReset( true ) {};
+};
+
+} // namespace sqlite
+} // db
+} // ou
