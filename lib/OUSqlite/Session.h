01559a82b6e33d1895ce430b9243b49a61750c54
Raymond Burkholder <raymond@burkholder.net>
2011-03-24 20:51:53 +0000

    Getting things to compile with g++
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@633 43143c16-083f-4785-a0a0-245a1fb241a9

--- lib/OUSqlite/Session.h
+++ lib/OUSqlite/Session.h
@@ -48,32 +48,32 @@ void CSession::Columns( typename QueryFields<F>::pQueryFields_t pQuery, C& colum
 }
 
 template<class F> 
-typename QueryFields<F>& CSession::RegisterTable( const std::string& sTableName ) {
+QueryFields<F>& CSession::RegisterTable( const std::string& sTableName ) {
   return pImpl->RegisterTable<F>( sTableName );
 }
 
 template<class F> 
-typename Query<F>& CSession::Insert( F& f ) {
+Query<F>& CSession::Insert( F& f ) {
   return pImpl->Insert<F>( f );
 }
 
 template<class F> 
-typename Query<F>& CSession::Update( F& f ) {
+Query<F>& CSession::Update( F& f ) {
   return pImpl->Update<F>( f );
 }
 
 template<class F> 
-typename Query<F>& CSession::Delete( F& f ) {
+Query<F>& CSession::Delete( F& f ) {
   return pImpl->Delete<F>( f );
 }
 
 template<class F> 
-typename Query< F>& CSession::SQL( const std::string& sSqlQuery, F& f ) {
+Query< F>& CSession::SQL( const std::string& sSqlQuery, F& f ) {
   return pImpl->SQL<F>( sSqlQuery, f );
 }
 
 template<class F> 
-typename Query< F>& CSession::SQL( const std::string& sSqlQuery ) {
+Query< F>& CSession::SQL( const std::string& sSqlQuery ) {
   return pImpl->SQL<F>( sSqlQuery );
 }
 
