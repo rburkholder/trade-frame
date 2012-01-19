30b250c4d14c478d4909fd262cd76fb1ded0224b
Raymond Burkholder <raymond@burkholder.net>
2012-01-19 03:35:14 +0000

    Adding missed gp files.
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@767 43143c16-083f-4785-a0a0-245a1fb241a9

---
+++ lib/OUGP/Individual.cpp
@@ -0,0 +1,24 @@
+/************************************************************************
+ * Copyright(c) 2012, One Unified. All rights reserved.                 *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+#include "GPIndividual.h"
+
+namespace ou { // One Unified
+
+GPIndividual::GPIndividual(void) {
+}
+
+GPIndividual::~GPIndividual(void) {
+}
+
+} // namespace ou
---
+++ lib/OUGP/Node.h
@@ -0,0 +1,26 @@
+/************************************************************************
+ * Copyright(c) 2012, One Unified. All rights reserved.                 *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+#pragma once
+
+namespace ou { // One Unified
+
+class GPNode {
+public:
+  GPNode(void);
+  ~GPNode(void);
+protected:
+private:
+};
+
+} // namespace ou
