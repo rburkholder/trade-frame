497d20ab18dbc7d4a251fa89042063ac62e107bb
Raymond Burkholder <raymond@burkholder.net>
2013-07-26 00:05:56 +0000

    Added TreePortfolioPositionOrder.
    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@898 43143c16-083f-4785-a0a0-245a1fb241a9

--- lib/TFVuTrading/PanelPortfolioPositionOrderExecution.cpp
+++ lib/TFVuTrading/PanelPortfolioPositionOrderExecution.cpp
@@ -1,10 +1,10 @@
-/************************************************************************
+ /************************************************************************
  * Copyright(c) 2011, One Unified. All rights reserved.                 *
  *                                                                      *
  * This file is provided as is WITHOUT ANY WARRANTY                     *
  *  without even the implied warranty of                                *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
- *                                                                      *
+ *                                                  8                    *
  * This software may not be used nor distributed without proper license *
  * agreement.                                                           *
  *                                                                      *
@@ -14,13 +14,14 @@
 #include "StdAfx.h"
 
 #include <wx/splitter.h>
+#include <wx/treectrl.h>
 
 #include "PanelPortfolioPositionOrderExecution.h"
 
 namespace ou { // One Unified
 namespace tf { // TradeFrame
 
-// need to fix this constructor for proper use of xwPanel inherit
+// need to fix this constructor for proper use of wxPanel inherit
 PanelPortfolioPositionOrderExecution::PanelPortfolioPositionOrderExecution(void) {
 }
 
@@ -30,27 +31,51 @@ PanelPortfolioPositionOrderExecution::~PanelPortfolioPositionOrderExecution(void
 void PanelPortfolioPositionOrderExecution::CreateControls( void ) {
 
   // size for encompassing panel
-  wxBoxSizer* pSizerPanelOuter = new wxBoxSizer( wxVERTICAL );
+  wxBoxSizer* pSizerPanelOuter = new wxBoxSizer( wxHORIZONTAL );
   SetSizer( pSizerPanelOuter );
 
+  // splitter for Left Tree and Right Tables
+  wxSplitterWindow* pSplitTreeAndTables = new wxSplitterWindow( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_BORDER );
+  pSplitTreeAndTables->SetMinimumPaneSize( 20 );
+  pSizerPanelOuter->Add( pSplitTreeAndTables, 1, wxEXPAND | wxALL, 5 );
+
+  // panel for tree
+  wxPanel* pPanelTree = new wxPanel( pSplitTreeAndTables, wxID_ANY );
+  wxBoxSizer* pSizerPanelTree = new wxBoxSizer( wxHORIZONTAL );
+  pPanelTree->SetSizer( pSizerPanelTree );
+
+  // panel for tables
+  wxPanel* pPanelTables = new wxPanel( pSplitTreeAndTables, wxID_ANY );
+  wxBoxSizer* pSizerPanelTables = new wxBoxSizer( wxHORIZONTAL );
+  pPanelTables->SetSizer( pSizerPanelTables );
+
+  // add panels to splitter
+  pSplitTreeAndTables->Initialize( pPanelTree );
+  pSplitTreeAndTables->SplitVertically( pPanelTree, pPanelTables );
+
+  // add tree to left panel, redo the object at some point
+  wxTreeCtrl* pTree = new wxTreeCtrl( pPanelTree, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTR_DEFAULT_STYLE | wxTR_ROW_LINES , wxDefaultValidator, "Test Tree" );
+  wxBoxSizer* pSizerTree = new wxBoxSizer( wxHORIZONTAL );
+  pTree->SetSizer( pSizerTree );
+
   // splitter for Portfolio/Position & Order/Execution Regions
-  wxSplitterWindow* pSplitOuter = new wxSplitterWindow( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_BORDER );
-  pSplitOuter->SetMinimumPaneSize( 20 );
-  pSizerPanelOuter->Add( pSplitOuter, 1, wxEXPAND | wxALL, 5 );
+  wxSplitterWindow* pSplitTablesOuter = new wxSplitterWindow( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_BORDER );
+  pSplitTablesOuter->SetMinimumPaneSize( 20 );
+  pSizerPanelTables->Add( pSplitTablesOuter, 1, wxEXPAND | wxALL, 5 );
 
   // panel a) Portfolio / Position
-  wxPanel* pPanelPortfolioPosition = new wxPanel( pSplitOuter, wxID_ANY );
+  wxPanel* pPanelPortfolioPosition = new wxPanel( pSplitTablesOuter, wxID_ANY );
   wxBoxSizer* pSizerPanelPortfolioPosition = new wxBoxSizer( wxVERTICAL );
   pPanelPortfolioPosition->SetSizer( pSizerPanelPortfolioPosition );
 
   // panel b) Order / Execution
-  wxPanel* pPanelOrderExecution = new wxPanel( pSplitOuter, wxID_ANY );
+  wxPanel* pPanelOrderExecution = new wxPanel( pSplitTablesOuter, wxID_ANY );
   wxBoxSizer* pSizerPanelOrderExecution = new wxBoxSizer( wxVERTICAL );
   pPanelOrderExecution->SetSizer( pSizerPanelOrderExecution );
 
   // add panels to outer splitter
-  pSplitOuter->Initialize( pPanelPortfolioPosition );
-  pSplitOuter->SplitHorizontally( pPanelPortfolioPosition, pPanelOrderExecution );
+  pSplitTablesOuter->Initialize( pPanelPortfolioPosition );
+  pSplitTablesOuter->SplitHorizontally( pPanelPortfolioPosition, pPanelOrderExecution );
 
   // splitter in panel a
   wxSplitterWindow* pSplitPortfolioPosition = new wxSplitterWindow( pPanelPortfolioPosition, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_BORDER );
