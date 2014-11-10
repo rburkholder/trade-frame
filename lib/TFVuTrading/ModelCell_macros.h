288facd438a9f1d83217d75724df2929c949bfc2
Raymond Burkholder <raymond@burkholder.net>
2014-11-10 03:22:26 +0000

    git-svn-id: svn+ssh://svn.oneunified.net/var/local/svn/private/projects/oneunified/projects/trunk/TradeFrame@1015 43143c16-083f-4785-a0a0-245a1fb241a9

---
+++ lib/TFVuTrading/ModelCell_macros.h
@@ -0,0 +1,64 @@
+/***********************************************************************
+ * Copyright(c) 2014, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+// Started 2014/09/21
+
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/array/elem.hpp>
+#include <boost/preprocessor/array/size.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/cat.hpp>
+
+#define GRID_EXTRACT_COL_DETAILS(z, row, col) \
+  BOOST_PP_TUPLE_ELEM( \
+    GRID_ARRAY_PARAM_COUNT, col, \
+      BOOST_PP_ARRAY_ELEM( row, GRID_ARRAY ) \
+    )
+
+// if n is 0, then no comma, ie, prepends comma except on first element, col is column number to extract
+#define GRID_EXTRACT_ENUM_LIST(z, n, col) \
+  BOOST_PP_COMMA_IF(n) \
+  GRID_EXTRACT_COL_DETAILS( z, n, col )
+
+#define GRID_EMIT_SetColSettings( z, n, VAR ) \
+  m_gridPositions->SetColLabelValue( VAR, _T(GRID_EXTRACT_COL_DETAILS(z, n, 1) ) ); \
+  m_gridPositions->SetColSize( VAR++, GRID_EXTRACT_COL_DETAILS(z, n, 3) );
+
+#define COL_ALIGNMENT( z, n, VAR ) \
+  m_pGrid->SetCellAlignment( VAR, GRID_EXTRACT_COL_DETAILS(z, n, 0), GRID_EXTRACT_COL_DETAILS(z, n, 2), wxALIGN_CENTRE );
+
+namespace ou { // One Unified
+namespace tf { // TradeFrame
+
+  template<typename ModelCell>
+  class CellInfo_t: public ModelCell {
+  public:
+    CellInfo_t( void ): m_col( 0 ) {};
+    CellInfo_t( int col ): m_col( col ) {};
+    virtual ~CellInfo_t( void ) {  }
+    int SetCol( int col ) { m_col = col; return m_col; }
+    int GetCol( void ) const { return m_col; }
+  private:
+    int m_col;
+  };
+
+} // namespace tf
+} // namespace ou
+
+#define COMPOSE_MODEL_CELL(z,n,col)\
+  BOOST_PP_COMMA_IF(n)\
+  CellInfo_t<GRID_EXTRACT_COL_DETAILS(z,n,col)>
+
+#define VECTOR_DEF BOOST_PP_CAT( vector, GRID_ARRAY_COL_COUNT )
+
---
+++ lib/TFVuTrading/ModelCell_ops.h
@@ -0,0 +1,57 @@
+/************************************************************************
+ * Copyright(c) 2014, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+// Started 2014/09/21
+
+#include <wx/grid.h>
+
+namespace ou { // One Unified
+namespace tf { // TradeFrame
+
+namespace ModelCell_ops {
+
+struct UpdateGui {
+  wxGrid* m_pGrid;
+  int m_row;
+  UpdateGui( wxGrid* pGrid, int row ): m_pGrid( pGrid ), m_row( row ) {};
+  template<typename T>
+  void operator()( T& t ) const {
+    // todo:  deal with flicker by double-buffering?
+    if ( t.Changed() ) {
+      m_pGrid->SetCellValue( t.GetText(), m_row, t.GetCol() );
+    }
+  }
+};
+
+struct SetCol {
+  typedef int result_type;
+  SetCol( void ) {};
+  template<typename F, typename T>
+  int operator()( F& f, T& t ) const {
+    return 1 + t.SetCol( f );
+  }
+};
+
+struct SetPrecision {
+  unsigned int m_nPrecision;
+  SetPrecision( unsigned int val ): m_nPrecision( val ) {};
+  template<typename T>
+  void operator()( T& t ) const {
+    t.SetPrecision( m_val );
+  }
+};
+
+}
+
+}
+}
