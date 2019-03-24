56e296243ff858158bded7da2549541ce6a717be
Raymond Burkholder <raymond@burkholder.net>
2019-03-23 21:16:11 -0600

    Weeklies/Weeklies - remove old template code

--- Weeklies/Weeklies.cpp
+++ Weeklies/Weeklies.cpp
@@ -16,24 +16,6 @@
 
 #include <iostream>
 
-#include <math.h>
-
-#include <boost/date_time/posix_time/posix_time.hpp>
-
-#include <boost/phoenix/bind/bind_member_function.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/bind.hpp>
-#include <boost/filesystem.hpp>
-#include <boost/timer/timer.hpp>
-
-#include <boost/foreach.hpp>
-
-#include <OUCommon/TimeSource.h>
-
-#include <TFTrading/InstrumentManager.h>
-#include <TFTrading/AccountManager.h>
-#include <TFTrading/OrderManager.h>
-
 #include "Weeklies.h"
 
 /*
@@ -67,28 +49,6 @@ bool AppWeeklies::OnInit() {
   m_sizerControls = new wxBoxSizer( wxHORIZONTAL );
   m_sizerMain->Add( m_sizerControls, 0, wxLEFT|wxTOP|wxRIGHT, 5 );
 
-  // populate variable in FrameWork01
-  m_pPanelProviderControl = new ou::tf::PanelProviderControl( m_pFrameMain, wxID_ANY );
-  m_sizerControls->Add( m_pPanelProviderControl, 0, wxEXPAND|wxALIGN_LEFT|wxRIGHT, 5);
-  m_pPanelProviderControl->Show( true );
-
-  LinkToPanelProviderControl();
-
-//  m_pPanelManualOrder = new ou::tf::PanelManualOrder( m_pFrameMain, wxID_ANY );
-//  m_sizerControls->Add( m_pPanelManualOrder, 0, wxEXPAND|wxALIGN_LEFT|wxRIGHT, 5);
-//  m_pPanelManualOrder->Show( true );
-
-/*
-  m_pPanelOptionsParameters = new PanelOptionsParameters( m_pFrameMain, wxID_ANY );
-  m_sizerControls->Add( m_pPanelOptionsParameters, 1, wxEXPAND|wxALIGN_LEFT, 0);
-  m_pPanelOptionsParameters->Show( true );
-  m_pPanelOptionsParameters->SetOnStart( MakeDelegate( this, &AppStrategyRunner::HandleBtnStart ) );
-  m_pPanelOptionsParameters->SetOnStop( MakeDelegate( this, &AppStrategyRunner::HandleBtnStop ) );
-  m_pPanelOptionsParameters->SetOnSave( MakeDelegate( this, &AppStrategyRunner::HandleBtnSave ) );
-  m_pPanelOptionsParameters->SetOptionNearDate( boost::gregorian::date( 2012, 4, 20 ) );
-  m_pPanelOptionsParameters->SetOptionFarDate( boost::gregorian::date( 2012, 6, 15 ) );
-*/
-
   wxBoxSizer* m_sizerStatus = new wxBoxSizer( wxHORIZONTAL );
   m_sizerMain->Add( m_sizerStatus, 1, wxEXPAND|wxALL, 5 );
 
@@ -98,35 +58,7 @@ bool AppWeeklies::OnInit() {
 
   m_pFrameMain->Show( true );
 
-  m_db.OnRegisterTables.Add( MakeDelegate( this, &AppWeeklies::HandleRegisterTables ) );
-  m_db.OnRegisterRows.Add( MakeDelegate( this, &AppWeeklies::HandleRegisterRows ) );
-  m_db.SetOnPopulateDatabaseHandler( MakeDelegate( this, &AppWeeklies::HandlePopulateDatabase ) );
-
-  m_bData1Connected = false;
-  m_bData2Connected = false;
-  m_bExecConnected = false;
-
-//  m_timerGuiRefresh.SetOwner( this );
-
-//  Bind( wxEVT_TIMER, &AppPhi::HandleGuiRefresh, this, m_timerGuiRefresh.GetId() );
-
   m_pFrameMain->Bind( wxEVT_CLOSE_WINDOW, &AppWeeklies::OnClose, this );  // start close of windows and controls
-
-//  m_pPanelManualOrder->SetOnNewOrderHandler( MakeDelegate( this, &AppPhi::HandlePanelNewOrder ) );
-//  m_pPanelManualOrder->SetOnSymbolTextUpdated( MakeDelegate( this, &AppPhi::HandlePanelSymbolText ) );
-//  m_pPanelManualOrder->SetOnFocusPropogate( MakeDelegate( this, &AppPhi::HandlePanelFocusPropogate ) );
-
-  // maybe set scenario with database and with in memory data structure
-  //m_idPortfolio = boost::gregorian::to_iso_string( boost::gregorian::day_clock::local_day() ) + "phi";
-//  m_idPortfolio = "weeklies";  // makes it easy for swing trading
-
-
-  std::string sDbName( "weeklies.db" );
-  if ( boost::filesystem::exists( sDbName ) ) {
-    boost::filesystem::remove( sDbName );
-  }
-  m_db.Open( sDbName );
-
 
   FrameMain::vpItems_t vItems;
   typedef FrameMain::structMenuItem mi;  // vxWidgets takes ownership of the objects
@@ -144,119 +76,19 @@ void AppWeeklies::HandleMenuActionRunScan( void ) {
   });
 }
 
-void AppWeeklies::HandleGuiRefresh( wxTimerEvent& event ) {
-  // update portfolio results and tracker timeseries for portfolio value
-//  double dblUnRealized;
-//  double dblRealized;
-//  double dblCommissionsPaid;
-/*  m_pPortfolio->QueryStats( dblUnRealized, dblRealized, dblCommissionsPaid );
-  double dblCurrent = dblUnRealized + dblRealized - dblCommissionsPaid;
-  m_dblMaxPL = std::max<double>( m_dblMaxPL, dblCurrent );
-  m_dblMinPL = std::min<double>( m_dblMinPL, dblCurrent );
-  m_pPanelPortfolioStats->SetStats(
-    boost::lexical_cast<std::string>( m_dblMinPL ),
-    boost::lexical_cast<std::string>( dblCurrent ),
-    boost::lexical_cast<std::string>( m_dblMaxPL )
-    );
-    */
-}
-
 int AppWeeklies::OnExit() {
   // Exit Steps: #4
-//  DelinkFromPanelProviderControl();  generates stack errors
-  //m_timerGuiRefresh.Stop();
-  m_listIQFeedSymbols.Clear();
-  if ( m_db.IsOpen() ) m_db.Close();
 
   return wxAppConsole::OnExit();
 }
-
-//void AppWeeklies::HandlePanelFocusPropogate( unsigned int ix ) {
-//}
-
 
 void AppWeeklies::OnClose( wxCloseEvent& event ) {
   // Exit Steps: #2 -> FrameMain::OnClose
   if ( m_worker.joinable() )
     m_worker.join();
-  m_timerGuiRefresh.Stop();
-  DelinkFromPanelProviderControl();
 //  if ( 0 != OnPanelClosing ) OnPanelClosing();
   // event.Veto();  // possible call, if needed
   // event.CanVeto(); // if not a
   event.Skip();  // auto followed by Destroy();
-}
-
-void AppWeeklies::OnData1Connected( int ) {
-  m_bData1Connected = true;
-  //ou::tf::libor::SetWatchOn( m_pData1Provider );
-//  m_libor.SetWatchOn( m_pData1Provider );
-//  AutoStartCollection();
-  if ( m_bData1Connected & m_bExecConnected ) {
-    // set start to enabled
-  }
-}
-
-void AppWeeklies::OnData2Connected( int ) {
-  m_bData2Connected = true;
-//  AutoStartCollection();
-  if ( m_bData2Connected & m_bExecConnected ) {
-    // set start to enabled
-  }
-}
-
-void AppWeeklies::OnExecConnected( int ) {
-  m_bExecConnected = true;
-  if ( m_bData1Connected & m_bExecConnected ) {
-    // set start to enabled
-  }
-}
-
-
-void AppWeeklies::OnData1Disconnected( int ) {
-  m_bData1Connected = false;
-}
-
-void AppWeeklies::OnData2Disconnected( int ) {
-  m_bData2Connected = false;
-}
-
-void AppWeeklies::OnExecDisconnected( int ) {
-  m_bExecConnected = false;
-}
-
-void AppWeeklies::HandleRegisterTables(  ou::db::Session& session ) {
-}
-
-void AppWeeklies::HandleRegisterRows(  ou::db::Session& session ) {
-}
-
-void AppWeeklies::HandlePopulateDatabase( void ) {
-/*
-
-  ou::tf::AccountManager::pAccountAdvisor_t pAccountAdvisor
-    = ou::tf::AccountManager::Instance().ConstructAccountAdvisor( "aaRay", "Raymond Burkholder", "One Unified" );
-
-  ou::tf::AccountManager::pAccountOwner_t pAccountOwner
-    = ou::tf::AccountManager::Instance().ConstructAccountOwner( "aoRay", "aaRay", "Raymond", "Burkholder" );
-
-  ou::tf::AccountManager::pAccount_t pAccountIB
-    = ou::tf::AccountManager::Instance().ConstructAccount( "ib01", "aoRay", "Raymond Burkholder", ou::tf::keytypes::EProviderIB, "Interactive Brokers", "acctid", "login", "password" );
-
-  ou::tf::AccountManager::pAccount_t pAccountIQFeed
-    = ou::tf::AccountManager::Instance().ConstructAccount( "iq01", "aoRay", "Raymond Burkholder", ou::tf::keytypes::EProviderIQF, "IQFeed", "acctid", "login", "password" );
-
-  ou::tf::AccountManager::pAccount_t pAccountSimulator
-    = ou::tf::AccountManager::Instance().ConstructAccount( "sim01", "aoRay", "Raymond Burkholder", ou::tf::keytypes::EProviderSimulator, "Sim", "acctid", "login", "password" );
-
-  m_pPortfolio
-    = ou::tf::PortfolioManager::Instance().ConstructPortfolio(
-    "Master", "aoRay", "", ou::tf::Portfolio::Master, ou::tf::Currency::Name[ ou::tf::Currency::USD ], "phi" );
-
-  m_pPortfolio
-    = ou::tf::PortfolioManager::Instance().ConstructPortfolio(
-    m_idPortfolio, "aoRay", "Master", ou::tf::Portfolio::CurrencySummary, ou::tf::Currency::Name[ ou::tf::Currency::USD ], "phi" );
-
-*/
 }
 
