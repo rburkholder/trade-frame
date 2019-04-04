017abcbbd355a9b181537ef7e56b5b938de31a02
Raymond Burkholder <raymond@burkholder.net>
2019-04-04 00:06:16 -0600

    sedgcc.sh - build for versioned boost

--- sedgcc.sh
+++ sedgcc.sh
@@ -1,24 +1,24 @@
 
-sed -i 's/-mt/-mt-x64/g' BasketTrading/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' BasketTrading/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' IQFeedMarketSymbols/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' IQFeedMarketSymbols/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' IQFeedGetHistory/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' IQFeedGetHistory/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' ComboTrading/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' ComboTrading/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' Weeklies/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' Weeklies/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' Hdf5Chart/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' Hdf5Chart/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' LiveChart/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' LiveChart/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' ArmsIndex/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' ArmsIndex/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' HedgedBollinger/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' HedgedBollinger/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' StickShift2/nbproject/Makefile*
-sed -i 's/-mt/-mt-x64/g' StickShift2/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' Scanner/nbproject/configurations.xml
-sed -i 's/-mt/-mt-x64/g' Scanner/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' BasketTrading/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' BasketTrading/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' IQFeedMarketSymbols/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' IQFeedMarketSymbols/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' IQFeedGetHistory/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' IQFeedGetHistory/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' ComboTrading/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' ComboTrading/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' Weeklies/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' Weeklies/nbproject/Makefile*
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' Hdf5Chart/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' Hdf5Chart/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' LiveChart/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' LiveChart/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' ArmsIndex/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' ArmsIndex/nbproject/configurations.xml
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' HedgedBollinger/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' HedgedBollinger/nbproject/Makefile*
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' StickShift2/nbproject/Makefile*
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' StickShift2/nbproject/configurations.xml
+sed -i 's/-mt-x64</-gcc8-mt-x64-1_69</g' Scanner/nbproject/configurations.xml
+sed -i 's/-mt-x64 /-gcc8-mt-x64-1_69 /g' Scanner/nbproject/Makefile*
 
