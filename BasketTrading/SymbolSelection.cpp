6d7b63ddd7e3810615cad891177eb00722d82f20
Raymond Burkholder <raymond@burkholder.net>
2019-05-11 21:59:36 -0600

    BasketTrading/SymbolSelection.cpp - widen search for appropriate option candidates

--- BasketTrading/SymbolSelection.cpp
+++ BasketTrading/SymbolSelection.cpp
@@ -104,8 +104,8 @@ void Process( ptime dtBegin, ptime dtEnd, size_t nMinBars, Function fCheck ) {
             ou::tf::Bars::const_iterator iterVolume = bars.end() - nMinBars;
             data.volumeEma = std::for_each( iterVolume, bars.end(), VolumeEma() );
             if ( ( 1000000 < data.volumeEma )
-              && ( 25.0 <=  bars.last().Close() )
-              && ( 98.0 >= bars.last().Close() ) 
+              && ( 30.0 <=  bars.last().Close() )
+              && ( 300.0 >= bars.last().Close() )
               && ( dtEnd.date() == bars.last().DateTime().date() )
               && ( 120 < bars.Size() )
               ) {
