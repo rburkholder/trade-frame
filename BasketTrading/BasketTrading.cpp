80ab3e330e7cdc3791a6656d2f50dd808b2f59d6
Raymond Burkholder <raymond@burkholder.net>
2019-05-13 07:59:29 -0600

    BasketTrading/BasketTrading.cpp - update working date

--- BasketTrading/BasketTrading.cpp
+++ BasketTrading/BasketTrading.cpp
@@ -47,7 +47,7 @@ bool AppBasketTrading::OnInit() {
 
   m_sDbName = "BasketTrading.db";
 
-  m_dtLatestEod = ptime( date( 2019, 5, 8 ), time_duration( 23, 59, 59 ) );
+  m_dtLatestEod = ptime( date( 2019, 5, 10 ), time_duration( 23, 59, 59 ) );
 
   m_pFrameMain = new FrameMain( 0, wxID_ANY, "Basket Trading" );
   wxWindowID idFrameMain = m_pFrameMain->GetId();
