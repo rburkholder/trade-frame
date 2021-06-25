f6280a4e23aea4046368b36529dec10ecea73e3a
Raymond Burkholder <raymond@burkholder.net>
2021-06-24 23:26:19 -0600

    lib/OUCharting/ChartEntryVolume.h - add #pragma once

--- lib/OUCharting/ChartEntryVolume.h
+++ lib/OUCharting/ChartEntryVolume.h
@@ -11,6 +11,8 @@
  * See the file LICENSE.txt for redistribution information.             *
  ************************************************************************/
 
+#pragma once
+
 #include <TFTimeSeries/DatedDatum.h>
 
 #include "ChartEntryPrice.h"
