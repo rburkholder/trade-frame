b9e129052064b4b158283974184e583197ab77be
Raymond Burkholder <raymond@burkholder.net>
2021-06-26 13:21:49 -0600

    lib/TFTrading/Symbol - const, void, typedef=>using

--- lib/TFTrading/Symbol.cpp
+++ lib/TFTrading/Symbol.cpp
@@ -11,15 +11,13 @@
  * See the file LICENSE.txt for redistribution information.             *
  ************************************************************************/
 
-#include "stdafx.h"
-
 #include "Symbol.h"
 
 namespace ou { // One Unified
 namespace tf { // TradeFrame
 
 SymbolBase::SymbolBase( pInstrument_t pInstrument )
-: 
+:
   m_id( pInstrument->GetInstrumentName() ),  // use the generic name, but in provider, use provider specific name
   m_pInstrument( pInstrument )
   {
@@ -27,7 +25,7 @@ SymbolBase::SymbolBase( pInstrument_t pInstrument )
 }
 
 SymbolBase::SymbolBase( pInstrument_t pInstrument, const std::string& sName )
-: 
+:
   m_id( sName ),  // use the generic name, but in provider, use provider specific name
   m_pInstrument( pInstrument )
   {
@@ -36,7 +34,7 @@ SymbolBase::SymbolBase( pInstrument_t pInstrument, const std::string& sName )
 SymbolBase::~SymbolBase(void) {
 }
 
-void SymbolBase::AddOnOpenHandler(tradehandler_t handler ) {
+void SymbolBase::AddOnOpenHandler( tradehandler_t handler ) {
   m_OnOpen.Add( handler );
 }
 
@@ -44,40 +42,40 @@ void SymbolBase::RemoveOnOpenHandler( tradehandler_t handler ) {
   m_OnOpen.Remove( handler );
 }
 
-bool SymbolBase::AddQuoteHandler(quotehandler_t handler) {
+bool SymbolBase::AddQuoteHandler( quotehandler_t handler ) {
   ou::Delegate<quote_t>::vsize_t size = m_OnQuote.Size();
   m_OnQuote.Add( handler );
   assert( size == ( m_OnQuote.Size() - 1 ) );
   return ( 1 == m_OnQuote.Size() );  // start watch for the symbol
 }
 
-bool SymbolBase::RemoveQuoteHandler(quotehandler_t handler) {
+bool SymbolBase::RemoveQuoteHandler( quotehandler_t handler ) {
   assert( 0 < m_OnQuote.Size() );
   m_OnQuote.Remove( handler );
   return ( 0 == m_OnQuote.Size() );  // no more so stop watch
 }
 
-bool SymbolBase::AddTradeHandler(tradehandler_t handler) {
+bool SymbolBase::AddTradeHandler( tradehandler_t handler ) {
   ou::Delegate<trade_t>::vsize_t size = m_OnTrade.Size();
   m_OnTrade.Add( handler );
   assert( size == ( m_OnTrade.Size() - 1 ) );
   return ( 1 == m_OnTrade.Size() ); // start watch on first handler
 }
 
-bool SymbolBase::RemoveTradeHandler(tradehandler_t handler) {
+bool SymbolBase::RemoveTradeHandler( tradehandler_t handler ) {
   assert( 0 < m_OnTrade.Size() );
   m_OnTrade.Remove( handler );
   return ( 0 == m_OnTrade.Size() ); // no more so stop watch
 }
 
-bool SymbolBase::AddDepthHandler(depthhandler_t handler) {
+bool SymbolBase::AddDepthHandler( depthhandler_t handler ) {
   ou::Delegate<depth_t>::vsize_t size = m_OnDepth.Size();
   m_OnDepth.Add( handler );
   assert( size == ( m_OnDepth.Size() - 1 ) );
   return ( 1 == m_OnDepth.Size() );  // when true, start watch
 }
 
-bool SymbolBase::RemoveDepthHandler(depthhandler_t handler) {
+bool SymbolBase::RemoveDepthHandler( depthhandler_t handler ) {
   assert( 0 < m_OnDepth.Size() );
   m_OnDepth.Remove( handler );
   return ( 0 == m_OnDepth.Size() );  // when true, stop watch
--- lib/TFTrading/Symbol.h
+++ lib/TFTrading/Symbol.h
@@ -23,7 +23,7 @@
 #include "Instrument.h"
 
 // change the Add/Remove...Handlers from virtual to CRTP?
-//  probably not, 
+//  probably not,
 
 namespace ou { // One Unified
 namespace tf { // TradeFrame
@@ -31,9 +31,9 @@ namespace tf { // TradeFrame
 class SymbolBase {
 public:
 
-  typedef Instrument::pInstrument_t pInstrument_t;
-  typedef Instrument::pInstrument_cref pInstrument_cref;
-  typedef Instrument::idInstrument_t symbol_id_t;
+  using pInstrument_t = Instrument::pInstrument_t;
+  using pInstrument_cref =  Instrument::pInstrument_cref;
+  using symbol_id_t = Instrument::idInstrument_t;
   //   may need to use provider specific name in alternate instrument name in Instrument
 
   // class should only be constructed with valid instrument, which already has general name as well as provider specific names
@@ -41,48 +41,48 @@ public:
   SymbolBase( pInstrument_t pInstrument, const std::string& sName );  // provider specific name
   virtual ~SymbolBase(void);
 
-  const symbol_id_t& GetId( void ) { return m_id; };  // used for mapping and indexing
-  pInstrument_t GetInstrument( void ) { return m_pInstrument; };
+  const symbol_id_t& GetId() const { return m_id; };  // used for mapping and indexing
+  pInstrument_t GetInstrument() { return m_pInstrument; };
 
-  typedef const Quote& quote_t;
-  typedef ou::Delegate<quote_t>::OnDispatchHandler quotehandler_t;
+  using quote_t = const Quote&;
+  using quotehandler_t = ou::Delegate<quote_t>::OnDispatchHandler;
 
-  typedef const Trade& trade_t;
-  typedef ou::Delegate<trade_t>::OnDispatchHandler tradehandler_t;
+  using trade_t = const Trade&;
+  using tradehandler_t = ou::Delegate<trade_t>::OnDispatchHandler;
 
-  typedef const MarketDepth& depth_t;
-  typedef ou::Delegate<depth_t>::OnDispatchHandler depthhandler_t;
+  using depth_t = const MarketDepth&;
+  using depthhandler_t = ou::Delegate<depth_t>::OnDispatchHandler;
 
-  typedef const Greek& greek_t;
-  typedef ou::Delegate<greek_t>::OnDispatchHandler greekhandler_t;
+  using greek_t = const Greek&;
+  using greekhandler_t = ou::Delegate<greek_t>::OnDispatchHandler;
 
   // these handlers are typically updated through the provider, rather than through client code
   bool AddQuoteHandler( quotehandler_t );
   bool RemoveQuoteHandler( quotehandler_t );
-  size_t GetQuoteHandlerCount( void ) { return m_OnQuote.Size(); };
+  size_t GetQuoteHandlerCount() const { return m_OnQuote.Size(); };
 
   void AddOnOpenHandler( tradehandler_t );
   void RemoveOnOpenHandler( tradehandler_t );
-  size_t GetOpenHandlerCount( void ) { return m_OnOpen.Size(); };
-  
-  bool AddTradeHandler( tradehandler_t ); 
+  size_t GetOpenHandlerCount() { return m_OnOpen.Size(); };
+
+  bool AddTradeHandler( tradehandler_t );
   bool RemoveTradeHandler( tradehandler_t );
-  size_t GetTradeHandlerCount( void ) { return m_OnTrade.Size(); };
+  size_t GetTradeHandlerCount() { return m_OnTrade.Size(); };
 
   bool AddDepthHandler( depthhandler_t );
   bool RemoveDepthHandler( depthhandler_t );
-  size_t GetDepthHandlerCount( void ) { return m_OnDepth.Size(); };
+  size_t GetDepthHandlerCount() { return m_OnDepth.Size(); };
 
   bool AddGreekHandler( greekhandler_t );
   bool RemoveGreekHandler( greekhandler_t );
-  size_t GetGreekHandlerCount( void ) { return m_OnGreek.Size(); };
+  size_t GetGreekHandlerCount() { return m_OnGreek.Size(); };
 
   // these are typically used by the provider only
-  bool  OpenWatchNeeded( void ) { return !m_OnOpen.IsEmpty(); };
-  bool QuoteWatchNeeded( void ) { return !m_OnQuote.IsEmpty(); };
-  bool TradeWatchNeeded( void ) { return !m_OnTrade.IsEmpty(); };
-  bool DepthWatchNeeded( void ) { return !m_OnDepth.IsEmpty(); };
-  bool GreekWatchNeeded( void ) { return !m_OnGreek.IsEmpty(); };
+  bool  OpenWatchNeeded() const { return !m_OnOpen.IsEmpty(); };
+  bool QuoteWatchNeeded() const { return !m_OnQuote.IsEmpty(); };
+  bool TradeWatchNeeded() const { return !m_OnTrade.IsEmpty(); };
+  bool DepthWatchNeeded() const { return !m_OnDepth.IsEmpty(); };
+  bool GreekWatchNeeded() const { return !m_OnGreek.IsEmpty(); };
 
 protected:
 
@@ -90,7 +90,7 @@ protected:
   pInstrument_t m_pInstrument;  // composition rather than inheritance as same instrument refers to different market data and order providers
 
   ou::Delegate<trade_t> m_OnOpen;  // first value upon market opening
-  ou::Delegate<quote_t> m_OnQuote; 
+  ou::Delegate<quote_t> m_OnQuote;
   ou::Delegate<trade_t> m_OnTrade;
   ou::Delegate<depth_t> m_OnDepth;
   ou::Delegate<greek_t> m_OnGreek;
@@ -102,7 +102,7 @@ private:
 template <typename S>  // S for Provider specific CRTP Symbol type
 class Symbol: public SymbolBase {
 public:
-  typedef boost::shared_ptr<S> pSymbol_t;
+  using pSymbol_t = boost::shared_ptr<S>;
   Symbol( pInstrument_t pInstrument ) : SymbolBase( pInstrument ) {};
   Symbol( pInstrument_t pInstrument, const std::string& sName ) : SymbolBase( pInstrument, sName ) {};
   virtual ~Symbol( void ) {};
