51f9e1d9e893874bc99837223f58269b66b82d0c
Raymond Burkholder <raymond@burkholder.net>
2022-02-15 18:58:32 -0700

    AutoTrade/Config.cpp - use ~ when symbol has #

--- AutoTrade/Config.cpp
+++ AutoTrade/Config.cpp
@@ -81,6 +81,7 @@ bool Load( const std::string& sFileName, Options& options ) {
       po::store( po::parse_config_file( ifs, config), vm );
 
       bOk |= parse<std::string>( sFileName, vm, sOption_Symbol, options.sSymbol );
+      std::replace_if( options.sSymbol.begin(), options.sSymbol.end(), [](char ch)->bool{return '~' == ch;}, '#' );
 
       bOk |= parse<int>( sFileName, vm, sOption_PeriodWidth, options.nPeriodWidth );
       bOk |= parse<int>( sFileName, vm, sOption_MA1Periods,  options.nMA1Periods );
