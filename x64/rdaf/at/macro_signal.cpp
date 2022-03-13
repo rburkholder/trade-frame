d38282ca0a8af56aab1633f64315114ebebb5154
Raymond Burkholder <raymond@burkholder.net>
2022-03-12 17:27:51 -0700

    x64/rdaf/at/macro_signal.cpp - expose analytics

--- x64/rdaf/at/macro_signal.cpp
+++ x64/rdaf/at/macro_signal.cpp
@@ -65,32 +65,42 @@ int macro_signal() {
     pTrades = pTreeTrades->GetBranch( "trade" );
   }
 
+  struct Trade {
+    double time;
+    double price;
+    uint64_t vol;
+    int64_t direction;
+  } trade;
+
+  pTrades->SetAddress( &trade );
+  int cntEntries = pTrades->GetEntries();
+  pTrades->GetEntry( cntEntries - 1 );
+
   //std::cout << "in macro_signal: " << pTreeTrades->GetEntries() << std::endl;
 
-/*
+
   //input of this function is the time, which is the current time
   //therefore we need to find a suitable range of bins along Y-axis (remember time is stored in y-axis)
   // and find the projection of h2. the result is a 1-D hist with x-axis being price
   // and value being volume
 
   //find the bin that the time given belongs to:
-  Int_t bin_y = h2->GetYaxis()->FindBin(time);
+  Int_t bin_y = pHisto2->GetYaxis()->FindBin( trade.time );
 
   //if bin_y is valid and larger than 1 then proceed, else abort (since there is not enought data)
   if (bin_y < 1) return -1;
 
   //now find projection of h2 from the beginning till now:
-  auto h2_x = h2->ProjectionX("_x", 1, bin_y);
+  auto h2_x = pHisto2->ProjectionX( "_x", 1, bin_y );
 
   // now that h2_x is calculated, fit a gaussian to the it (i.e volume distribution)
-  auto b = h2_x->Fit("gaus","S");
+  auto b = h2_x->Fit( "gaus", "S" );
 
   //if fit is valid proceed, else abort
-  if (!b->IsValid()) return -1;
+  if ( !b->IsValid() ) return -1;
 
   //finally, if price is within 1 sigma from the mean, it's a good signal and go long:
-  if (abs( price - b->Parameter(1)) < b->Parameter(2)) return 1;
-*/
+  if ( abs( trade.price - b->Parameter(1)) < b->Parameter(2) ) return 1;
 
   return 0;
 
