83eca65071a6e39167989f361adb7a7ff34886cc
Raymond Burkholder <raymond@burkholder.net>
2022-04-14 12:29:36 -0600

    lib/TFVuTrading/MarketDepth/WinRow.cpp - fix spacing

--- lib/TFVuTrading/MarketDepth/WinRow.cpp
+++ lib/TFVuTrading/MarketDepth/WinRow.cpp
@@ -53,13 +53,13 @@ namespace { // local variables
 
   using vElement_t = std::vector<Element>;
   vElement_t vElement = {
-     { EField::BuyCount,   50, "Ticks",   wxCENTER, EColour::LightSkyBlue,  EColour::Black, EColour::PaleGoldenrod  }
-   , { EField::BuyVolume,  50, "Bought",  wxCENTER, EColour::LightSkyBlue,  EColour::Black, EColour::PaleGoldenrod  }
-   , { EField::BidSize,    50, "BidSize", wxCENTER, EColour::LightSkyBlue,  EColour::Black, EColour::DodgerBlue  }
-   , { EField::Price,      60, "Price",   wxCENTER, EColour::LightSeaGreen, EColour::Black, EColour::LightYellow  }
-   , { EField::AskSize,    50, "AskSize", wxCENTER, EColour::LightPink,     EColour::Black, EColour::Magenta      }
-   , { EField::SellVolume, 50, "Sold",    wxCENTER, EColour::LightPink,     EColour::Black, EColour::PaleGoldenrod      }
-   , { EField::SellCount,  50, "Ticks",   wxCENTER, EColour::LightPink,     EColour::Black, EColour::PaleGoldenrod      }
+     { EField::BuyCount,   50, "Ticks",   wxCENTER, EColour::LightSkyBlue,  EColour::Black, EColour::PaleGoldenrod }
+   , { EField::BuyVolume,  50, "Bought",  wxCENTER, EColour::LightSkyBlue,  EColour::Black, EColour::PaleGoldenrod }
+   , { EField::BidSize,    50, "BidSize", wxCENTER, EColour::LightSkyBlue,  EColour::Black, EColour::DodgerBlue    }
+   , { EField::Price,      60, "Price",   wxCENTER, EColour::LightSeaGreen, EColour::Black, EColour::LightYellow   }
+   , { EField::AskSize,    50, "AskSize", wxCENTER, EColour::LightPink,     EColour::Black, EColour::Magenta       }
+   , { EField::SellVolume, 50, "Sold",    wxCENTER, EColour::LightPink,     EColour::Black, EColour::PaleGoldenrod }
+   , { EField::SellCount,  50, "Ticks",   wxCENTER, EColour::LightPink,     EColour::Black, EColour::PaleGoldenrod }
    , { EField::Ticks,      50, "Ticks",   wxCENTER, EColour::DimGray,       EColour::White, EColour::PaleGoldenrod } // count of trades
    , { EField::Volume,     60, "Vol",     wxCENTER, EColour::DimGray,       EColour::White, EColour::PaleGoldenrod } // sum of volume
    , { EField::Static,     80, "SttcInd", wxLEFT,   EColour::DimGray,       EColour::White, EColour::PaleGoldenrod } // static indicators - pivots, ...
