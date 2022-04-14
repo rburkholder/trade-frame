4891adf63f0d07889dac0efe57d6f419e86a5c61
Raymond Burkholder <raymond@burkholder.net>
2022-04-14 12:30:33 -0600

    DepthOfMarket/PanelStatistics.cpp - improve readability

--- DepthOfMarket/PanelStatistics.cpp
+++ DepthOfMarket/PanelStatistics.cpp
@@ -74,11 +74,11 @@ void PanelStatistics::CreateControls() {
     wxStaticText* itemStaticText1 = new wxStaticText( itemPanel1, wxID_STATIC, wxEmptyString, wxDefaultPosition, wxSize(55, -1), 0 );
     itemBoxSizer1->Add(itemStaticText1, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText2 = new wxStaticText( itemPanel1, wxID_STATIC, _("ticks"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer1->Add(itemStaticText2, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
-
     m_txtTicks = new wxTextCtrl( itemPanel1, id_txtTicks, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer1->Add(m_txtTicks, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+
+    wxStaticText* itemStaticText4 = new wxStaticText( itemPanel1, wxID_STATIC, _("ticks"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer1->Add(itemStaticText4, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     wxBoxSizer* itemBoxSizer4 = new wxBoxSizer(wxHORIZONTAL);
     itemBoxSizer2->Add(itemBoxSizer4, 0, wxALIGN_LEFT|wxALL, 1);
@@ -86,23 +86,23 @@ void PanelStatistics::CreateControls() {
     wxStaticText* itemStaticText5 = new wxStaticText( itemPanel1, wxID_STATIC, _("l1 msgs:"), wxDefaultPosition, wxSize(55, -1), 0 );
     itemBoxSizer4->Add(itemStaticText5, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText6 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer4->Add(itemStaticText6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
-
     m_txtL1MsgBid = new wxTextCtrl( itemPanel1, id_txtL1MsgBid, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer4->Add(m_txtL1MsgBid, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText8 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer4->Add(itemStaticText8, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText3 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer4->Add(itemStaticText3, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL1MsgAsk = new wxTextCtrl( itemPanel1, id_txtL1MsgAsk, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer4->Add(m_txtL1MsgAsk, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText10 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer4->Add(itemStaticText10, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText6 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer4->Add(itemStaticText6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL1MsgTtl = new wxTextCtrl( itemPanel1, id_txtL1MsgTtl, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer4->Add(m_txtL1MsgTtl, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+
+    wxStaticText* itemStaticText8 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer4->Add(itemStaticText8, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     wxBoxSizer* itemBoxSizer12 = new wxBoxSizer(wxHORIZONTAL);
     itemBoxSizer2->Add(itemBoxSizer12, 0, wxALIGN_LEFT|wxALL, 1);
@@ -110,23 +110,23 @@ void PanelStatistics::CreateControls() {
     wxStaticText* itemStaticText13 = new wxStaticText( itemPanel1, wxID_STATIC, _("l2 msgs:"), wxDefaultPosition, wxSize(55, -1), 0 );
     itemBoxSizer12->Add(itemStaticText13, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText14 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer12->Add(itemStaticText14, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
-
     m_txtL2MsgBid = new wxTextCtrl( itemPanel1, id_txtL2MsgBid, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer12->Add(m_txtL2MsgBid, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText16 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer12->Add(itemStaticText16, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText10 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer12->Add(itemStaticText10, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL2MsgAsk = new wxTextCtrl( itemPanel1, id_txtL2MsgAsk, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer12->Add(m_txtL2MsgAsk, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText18 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer12->Add(itemStaticText18, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText12 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer12->Add(itemStaticText12, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL2MsgTtl = new wxTextCtrl( itemPanel1, id_txtL2MsgTtl, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer12->Add(m_txtL2MsgTtl, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+
+    wxStaticText* itemStaticText14 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer12->Add(itemStaticText14, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     wxBoxSizer* itemBoxSizer20 = new wxBoxSizer(wxHORIZONTAL);
     itemBoxSizer2->Add(itemBoxSizer20, 0, wxALIGN_LEFT|wxALL, 1);
@@ -134,23 +134,23 @@ void PanelStatistics::CreateControls() {
     wxStaticText* itemStaticText21 = new wxStaticText( itemPanel1, wxID_STATIC, _("l2 add:"), wxDefaultPosition, wxSize(55, -1), 0 );
     itemBoxSizer20->Add(itemStaticText21, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText22 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer20->Add(itemStaticText22, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
-
     m_txtL2AddBid = new wxTextCtrl( itemPanel1, id_txtL2AddBid, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer20->Add(m_txtL2AddBid, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText24 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer20->Add(itemStaticText24, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText16 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer20->Add(itemStaticText16, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL2AddAsk = new wxTextCtrl( itemPanel1, id_txtL2AddAsk, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer20->Add(m_txtL2AddAsk, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText26 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer20->Add(itemStaticText26, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText18 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer20->Add(itemStaticText18, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL2AddTtl = new wxTextCtrl( itemPanel1, id_txtL2AddTtl, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer20->Add(m_txtL2AddTtl, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+
+    wxStaticText* itemStaticText20 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer20->Add(itemStaticText20, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     wxBoxSizer* itemBoxSizer28 = new wxBoxSizer(wxHORIZONTAL);
     itemBoxSizer2->Add(itemBoxSizer28, 0, wxALIGN_LEFT|wxALL, 1);
@@ -158,23 +158,23 @@ void PanelStatistics::CreateControls() {
     wxStaticText* itemStaticText29 = new wxStaticText( itemPanel1, wxID_STATIC, _("l2 del:"), wxDefaultPosition, wxSize(55, -1), 0 );
     itemBoxSizer28->Add(itemStaticText29, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText30 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer28->Add(itemStaticText30, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
-
     m_txtL2DelBid = new wxTextCtrl( itemPanel1, id_txtL2DelBid, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer28->Add(m_txtL2DelBid, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText32 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer28->Add(itemStaticText32, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText22 = new wxStaticText( itemPanel1, wxID_STATIC, _("bid"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer28->Add(itemStaticText22, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL2DelAsk = new wxTextCtrl( itemPanel1, id_txtL2DelAsk, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer28->Add(m_txtL2DelAsk, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
-    wxStaticText* itemStaticText34 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer28->Add(itemStaticText34, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxStaticText* itemStaticText24 = new wxStaticText( itemPanel1, wxID_STATIC, _("ask"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer28->Add(itemStaticText24, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
     m_txtL2DelTtl = new wxTextCtrl( itemPanel1, id_txtL2DelTtl, wxEmptyString, wxDefaultPosition, wxSize(50, -1), wxTE_READONLY|wxTE_RIGHT|wxNO_BORDER );
     itemBoxSizer28->Add(m_txtL2DelTtl, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+
+    wxStaticText* itemStaticText26 = new wxStaticText( itemPanel1, wxID_STATIC, _("ttl"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer28->Add(itemStaticText26, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
 
 }
 
