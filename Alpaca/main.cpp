0d20b5713ac8d1e402d50f457e5c8fed6e6749c8
Raymond Burkholder <raymond@burkholder.net>
2022-06-05 14:01:17 -0600

    Alpaca/main.cpp - retrieve/decode single asset

--- Alpaca/main.cpp
+++ Alpaca/main.cpp
@@ -19,11 +19,15 @@
 #include <iostream>
 #include <functional>
 
+
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/version.hpp>
+
 #include <boost/asio/strand.hpp>
+
+#include <boost/json.hpp>
 
 #include <TFAlpaca/root_certificates.hpp>
 
@@ -33,16 +37,54 @@ namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+namespace json = boost::json;           // from <boost/json.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 //------------------------------------------------------------------------------
 
 // Report a failure
-void
-fail(beast::error_code ec, char const* what)
-{
+void fail(beast::error_code ec, char const* what) {
     std::cerr << what << ": " << ec.message() << "\n";
 }
+
+namespace alpaca {
+
+  // https://alpaca.markets/docs/api-references/trading-api/assets/
+  struct Asset {
+    std::string id;
+    std::string class_;
+    std::string exchange;
+    std::string symbol;
+    std::string status;
+    bool        tradable;
+    bool        marginable;
+    bool        shortable;
+    bool        easy_to_borrow;
+    bool        fractionable;
+  };
+
+  template<class T>
+  void extract( json::object const& obj, T& t, json::string_view key ) {
+      t = json::value_to<T>( obj.at( key ) );
+  }
+
+  Asset tag_invoke( json::value_to_tag<Asset>, json::value const& jv ) {
+    Asset asset;
+    json::object const& obj = jv.as_object();
+    extract( obj, asset.id, "id" );
+    extract( obj, asset.class_, "class" );
+    extract( obj, asset.exchange, "exchange" );
+    extract( obj, asset.symbol, "symbol" );
+    extract( obj, asset.status, "status" );
+    extract( obj, asset.tradable, "tradable" );
+    extract( obj, asset.marginable, "marginable" );
+    extract( obj, asset.shortable, "shortable" );
+    extract( obj, asset.easy_to_borrow, "easy_to_borrow" );
+    extract( obj, asset.fractionable, "fractionable" );
+    return asset;
+  }
+
+} // namespace asset
 
 // Performs an HTTP GET and prints the response
 class session : public std::enable_shared_from_this<session>
@@ -54,151 +96,156 @@ class session : public std::enable_shared_from_this<session>
     http::response<http::string_body> res_;
 
 public:
-    explicit session(
-        net::any_io_executor ex,
-        ssl::context& ctx)
-    : resolver_(ex)
-    , stream_(ex, ctx)
+  explicit session(
+      net::any_io_executor ex,
+      ssl::context& ctx)
+  : resolver_(ex)
+  , stream_(ex, ctx)
+  {
+  }
+
+  // Start the asynchronous operation
+  void run(
+      const std::string& sHost,
+      const std::string& sPort,
+      const std::string& sTarget,
+      int version,
+      const std::string& sAlpacaKey,
+      const std::string& sAlpacaSecret
+  ) {
+      // Set SNI Hostname (many hosts need this to handshake successfully)
+      if(! SSL_set_tlsext_host_name(stream_.native_handle(), sHost.c_str()))
+      {
+          beast::error_code ec{static_cast<int>(::ERR_get_error()), net::error::get_ssl_category()};
+          std::cerr << ec.message() << "\n";
+          return;
+      }
+
+      // Set up an HTTP GET request message
+      req_.version( version );
+      req_.method( http::verb::get );
+      req_.target( sTarget );
+      req_.set( http::field::host, sHost );
+      req_.set( http::field::user_agent, BOOST_BEAST_VERSION_STRING );
+      req_.set( "APCA-API-KEY-ID", sAlpacaKey );
+      req_.set( "APCA-API-SECRET-KEY", sAlpacaSecret );
+
+      // Look up the domain name
+      resolver_.async_resolve(
+          sHost,
+          sPort,
+          beast::bind_front_handler(
+              &session::on_resolve,
+              shared_from_this()));
+  }
+
+  void on_resolve(
+      beast::error_code ec,
+      tcp::resolver::results_type results)
+  {
+      if(ec)
+          return fail(ec, "resolve");
+
+      // Set a timeout on the operation
+      beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+
+      // Make the connection on the IP address we get from a lookup
+      beast::get_lowest_layer(stream_).async_connect(
+          results,
+          beast::bind_front_handler(
+              &session::on_connect,
+              shared_from_this()));
+  }
+
+  void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type)
+  {
+      if(ec)
+          return fail(ec, "connect");
+
+      // Perform the SSL handshake
+      stream_.async_handshake(
+          ssl::stream_base::client,
+          beast::bind_front_handler(
+              &session::on_handshake,
+              shared_from_this()));
+  }
+
+  void on_handshake(beast::error_code ec)
+  {
+      if(ec)
+          return fail(ec, "handshake");
+
+      // Set a timeout on the operation
+      beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+
+      // Send the HTTP request to the remote host
+      http::async_write(stream_, req_,
+          beast::bind_front_handler(
+              &session::on_write,
+              shared_from_this()));
+  }
+
+  void on_write(
+      beast::error_code ec,
+      std::size_t bytes_transferred)
+  {
+      boost::ignore_unused(bytes_transferred);
+
+      if(ec)
+          return fail(ec, "write");
+
+      // Receive the HTTP response
+      http::async_read(stream_, buffer_, res_,
+          beast::bind_front_handler(
+              &session::on_read,
+              shared_from_this()));
+  }
+
+  void on_read( beast::error_code ec, std::size_t bytes_transferred ) {
+
+    boost::ignore_unused(bytes_transferred);
+
+    if(ec)
+        return fail(ec, "read");
+
+    // Write the message to standard out
+    //std::cout << res_ << std::endl;
+
+    std::cout << res_.body() << std::endl;
+
+
+
+    json::error_code ec2;
+    json::value jv = json::parse( res_.body(), ec2 );
+    if ( ec2.failed() ) {
+      return fail( ec2, "parse" );
+    }
+
+    alpaca::Asset asset( json::value_to<alpaca::Asset>( jv ) );
+
+
+    // Set a timeout on the operation
+    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+
+    // Gracefully close the stream
+    stream_.async_shutdown(
+        beast::bind_front_handler(
+            &session::on_shutdown,
+            shared_from_this()));
+  }
+
+  void on_shutdown(beast::error_code ec) {
+    if(ec == net::error::eof)
     {
+        // Rationale:
+        // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+        ec = {};
     }
+    if(ec)
+        return fail(ec, "shutdown");
 
-    // Start the asynchronous operation
-    void run(
-        const std::string& sHost,
-        const std::string& sPort,
-        const std::string& sTarget,
-        int version,
-        const std::string& sAlpacaKey,
-        const std::string& sAlpacaSecret
-    ) {
-        // Set SNI Hostname (many hosts need this to handshake successfully)
-        if(! SSL_set_tlsext_host_name(stream_.native_handle(), sHost.c_str()))
-        {
-            beast::error_code ec{static_cast<int>(::ERR_get_error()), net::error::get_ssl_category()};
-            std::cerr << ec.message() << "\n";
-            return;
-        }
-
-        // Set up an HTTP GET request message
-        req_.version(version);
-        req_.method(http::verb::get);
-        req_.target(sTarget);
-        req_.set(http::field::host, sHost);
-        req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-        req_.set( "APCA-API-KEY-ID", sAlpacaKey );
-        req_.set( "APCA-API-SECRET-KEY", sAlpacaSecret );
-
-        // Look up the domain name
-        resolver_.async_resolve(
-            sHost,
-            sPort,
-            beast::bind_front_handler(
-                &session::on_resolve,
-                shared_from_this()));
-    }
-
-    void on_resolve(
-        beast::error_code ec,
-        tcp::resolver::results_type results)
-    {
-        if(ec)
-            return fail(ec, "resolve");
-
-        // Set a timeout on the operation
-        beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
-
-        // Make the connection on the IP address we get from a lookup
-        beast::get_lowest_layer(stream_).async_connect(
-            results,
-            beast::bind_front_handler(
-                &session::on_connect,
-                shared_from_this()));
-    }
-
-    void
-    on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type)
-    {
-        if(ec)
-            return fail(ec, "connect");
-
-        // Perform the SSL handshake
-        stream_.async_handshake(
-            ssl::stream_base::client,
-            beast::bind_front_handler(
-                &session::on_handshake,
-                shared_from_this()));
-    }
-
-    void
-    on_handshake(beast::error_code ec)
-    {
-        if(ec)
-            return fail(ec, "handshake");
-
-        // Set a timeout on the operation
-        beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
-
-        // Send the HTTP request to the remote host
-        http::async_write(stream_, req_,
-            beast::bind_front_handler(
-                &session::on_write,
-                shared_from_this()));
-    }
-
-    void
-    on_write(
-        beast::error_code ec,
-        std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
-        if(ec)
-            return fail(ec, "write");
-
-        // Receive the HTTP response
-        http::async_read(stream_, buffer_, res_,
-            beast::bind_front_handler(
-                &session::on_read,
-                shared_from_this()));
-    }
-
-    void
-    on_read(
-        beast::error_code ec,
-        std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
-        if(ec)
-            return fail(ec, "read");
-
-        // Write the message to standard out
-        std::cout << res_ << std::endl;
-
-        // Set a timeout on the operation
-        beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
-
-        // Gracefully close the stream
-        stream_.async_shutdown(
-            beast::bind_front_handler(
-                &session::on_shutdown,
-                shared_from_this()));
-    }
-
-    void
-    on_shutdown(beast::error_code ec)
-    {
-        if(ec == net::error::eof)
-        {
-            // Rationale:
-            // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
-            ec = {};
-        }
-        if(ec)
-            return fail(ec, "shutdown");
-
-        // If we get here then the connection is closed gracefully
-    }
+    // If we get here then the connection is closed gracefully
+  }
 };
 
 //------------------------------------------------------------------------------
