2d49b0a3f5f103959bc84dba4239728d66d8ac61
Raymond Burkholder <raymond@burkholder.net>
2022-07-19 21:46:10 -0600

    lib/OUCommon/ConsoleStream.h - add asserts, use the lock helper

--- lib/OUCommon/ConsoleStream.h
+++ lib/OUCommon/ConsoleStream.h
@@ -32,10 +32,10 @@ template <typename charT, typename traits = std::char_traits<charT> >
 class ConsoleStreamBuf:  public std::basic_streambuf<charT, traits> {
 public:
 
-  typedef traits traits_type;
-  typedef typename traits_type::int_type int_type;
-  typedef typename traits_type::pos_type pos_type;
-  typedef typename traits_type::off_type off_type;
+  using traits_type = traits;
+  using int_type = typename traits_type::int_type;
+  using pos_type = typename traits_type::pos_type;
+  using off_type = typename traits_type::off_type;
 
   static const size_t nCharT = 512;
 
@@ -64,7 +64,7 @@ protected:
 private:
 
   //charT m_buf[ nBufSize ]; // arbitrary length sized to get most console length stuff
-  Buf* m_pBuf;
+  Buf* m_pBuf; // 'current' buffer in use
   BufferRepository<Buf> m_buffers;
 
   OnEmitStringHandler OnEmitString;
@@ -72,17 +72,22 @@ private:
 };
 
 template <typename charT, typename traits>
-ConsoleStreamBuf<charT,traits>::ConsoleStreamBuf(): std::basic_streambuf<charT, traits>() {
+ConsoleStreamBuf<charT,traits>::ConsoleStreamBuf()
+: std::basic_streambuf<charT, traits>()
+, m_pBuf( nullptr )
+{
   m_pBuf = m_buffers.CheckOutL();
   this->setp( m_pBuf->buf, m_pBuf->buf + nCharT - 1 );  // allow for one of our own terminating characters
 }
 
 template <typename charT, typename traits>
 ConsoleStreamBuf<charT,traits>::~ConsoleStreamBuf() {
+  assert( m_pBuf );
   if ( 0 != m_pBuf->n ) {
     m_pBuf->n = 0;
   }
   m_buffers.CheckInL( m_pBuf );
+  m_pBuf = nullptr;
 }
 
 template <typename charT, typename traits>
@@ -95,24 +100,28 @@ template <typename charT, typename traits>
 typename ConsoleStreamBuf<charT, traits>::int_type
 ConsoleStreamBuf<charT,traits>::overflow( typename ConsoleStreamBuf<charT, traits>::int_type meta ) {
 
-//  charT* ibegin = this->out_buf_;
-  charT* iend = this->pptr();
+  m_buffers.ScopedLock(
+    [this,&meta](){
+      //  charT* ibegin = this->out_buf_;
+      charT* iend = this->pptr();
 
-  // If this is the end, add an eof character to the buffer.
-  // This is why the pointers passed to setp are off by 1
-  // (to reserve room for this).
-  if( !traits_type::eq_int_type( meta, traits_type::eof() ) ) {
-      *iend++ = traits_type::to_char_type( meta );
-  }
-
-  m_pBuf->n = iend - this->pbase();
-  //if ( NULL != OnNewString ) OnNewString( pbase(), (int) ( pptr() - pbase() - 1 ) ); // assumes CR at end
-  if ( NULL != OnEmitString ) OnEmitString( m_pBuf );
-  //if ( NULL != OnFlushString ) OnFlushString();
-  //setp( pbase(), epptr() );  // had this first but following line just resets everything.  better?  does the same thing?
-  m_pBuf = m_buffers.CheckOutL();
-  this->setp( m_pBuf->buf, m_pBuf->buf + nCharT - 1 );  // allow for one of our own terminating characters
-//  if ( NULL != OnFlushString ) OnFlushString();
+      // If this is the end, add an eof character to the buffer.
+      // This is why the pointers passed to setp are off by 1
+      // (to reserve room for this).
+      if( !traits_type::eq_int_type( meta, traits_type::eof() ) ) {
+          *iend++ = traits_type::to_char_type( meta );
+      }
+      m_pBuf->n = iend - this->pbase();
+      //if ( nullptr != OnNewString ) OnNewString( pbase(), (int) ( pptr() - pbase() - 1 ) ); // assumes CR at end
+      if ( nullptr != OnEmitString ) OnEmitString( m_pBuf );
+      //if ( nullptr != OnFlushString ) OnFlushString();
+      //setp( pbase(), epptr() );  // had this first but following line just resets everything.  better?  does the same thing?
+      //m_pBuf = m_buffers.CheckOutL();
+      m_pBuf = m_buffers.CheckOut();
+      this->setp( m_pBuf->buf, m_pBuf->buf + nCharT - 1 );  // allow for one of our own terminating characters
+    //  if ( nullptr != OnFlushString ) OnFlushString();
+    }
+  );
 
   return traits_type::not_eof( meta );
 
