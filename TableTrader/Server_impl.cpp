81cf7be490c1d1ea08dcd3efa68137dfd44e3c62
Raymond Burkholder <raymond@burkholder.net>
2022-08-04 11:27:23 -0600

    TableTrader/Server_impl - startup providers

--- TableTrader/Server_impl.cpp
+++ TableTrader/Server_impl.cpp
@@ -19,6 +19,8 @@
  * Created:   2022/08/03 17:14:41
  */
 
+#include <boost/log/trivial.hpp>
+
 #include <TFTrading/DBWrapper.h>
 #include <TFTrading/OrderManager.h>
 #include <TFTrading/PortfolioManager.h>
@@ -30,6 +32,80 @@
 
 #include "Server_impl.hpp"
 
-Server_impl::Server_impl() {}
+namespace {
+  static const std::string sDataBaseName( "TableTrader.db" );
+}
 
-Server_impl::~Server_impl() {}
+Server_impl::Server_impl()
+: m_state( EState::quiescent )
+{
+
+  ou::tf::ProviderManager& providers( ou::tf::ProviderManager::GlobalInstance() );
+
+  //using pProviderTWS_t = ou::tf::ib::TWS::pProvider_t;
+  m_pProviderTWS = ou::tf::ib::TWS::Factory();
+  m_pProviderTWS->SetName( "tws" ); // needs to match name in database
+  providers.Register( m_pProviderTWS );
+  m_pProviderTWS->OnConnected.Add( MakeDelegate( this, &Server_impl::Connected_TWS ) );
+  m_pProviderTWS->OnDisconnected.Add( MakeDelegate( this, &Server_impl::Disconnected_TWS ) );
+
+  //using pProviderIQFeed_t = ou::tf::iqfeed::IQFeedProvider::pProvider_t;
+  m_pProviderIQFeed = ou::tf::iqfeed::IQFeedProvider::Factory();
+  m_pProviderIQFeed->SetName( "iqf" ); // needs to match name in database
+  providers.Register( m_pProviderIQFeed );
+  m_pProviderIQFeed->OnConnected.Add( MakeDelegate( this, &Server_impl::Connected_IQFeed ) );
+  m_pProviderIQFeed->OnDisconnected.Add( MakeDelegate( this, &Server_impl::Disconnected_IQFeed ) );
+
+  assert( m_pProviderTWS );
+  assert( m_pProviderIQFeed );
+
+  m_pdb = std::make_unique<ou::tf::db>( sDataBaseName );
+
+  m_pProviderTWS->Connect();
+  m_pProviderIQFeed->Connect();
+
+  m_state = EState::connecting;
+
+}
+
+Server_impl::~Server_impl() {
+  m_pProviderTWS->Disconnect();
+  m_pProviderIQFeed->Disconnect();
+}
+
+void Server_impl::Connected_TWS( int n ) {
+  BOOST_LOG_TRIVIAL(info) << "Provider TWS connected";
+  Connected( n );
+}
+
+void Server_impl::Connected_IQFeed( int n ) {
+  BOOST_LOG_TRIVIAL(info) << "Provider IQFeed connected";
+  Connected( n );
+}
+
+void Server_impl::Connected( int ) {
+  if ( m_pProviderTWS->Connected() && m_pProviderIQFeed->Connected() ) {
+    m_state = EState::connected;
+    // TODO: generate signal or status to interface
+  }
+}
+
+void Server_impl::Disconnected_TWS( int n ) {
+  BOOST_LOG_TRIVIAL(info) << "Provider TWS disconnected";
+  Disconnected( n );
+}
+
+void Server_impl::Disconnected_IQFeed( int n ) {
+  BOOST_LOG_TRIVIAL(info) << "Provider IQFeed disconnected";
+  Disconnected( n );
+}
+
+void Server_impl::Disconnected( int ) {
+  if ( !m_pProviderTWS->Connected() && !m_pProviderIQFeed->Connected() ) {
+    m_state = EState::disconnected;
+  }
+}
+
+void Server_impl::Start( const std::string& sUnderlyingFuture ) {
+
+}
--- TableTrader/Server_impl.hpp
+++ TableTrader/Server_impl.hpp
@@ -21,10 +21,43 @@
 
 #pragma once
 
+#include <memory>
+#include <string>
+
+namespace ou {
+namespace tf {
+  class db;
+namespace iqfeed {
+  class IQFeedProvider;
+}
+namespace ib {
+  class TWS;
+}
+}
+}
+
 class Server_impl {
 public:
   Server_impl();
   ~Server_impl();
+
+  void Start( const std::string& sUnderlyingFuture );
 protected:
 private:
+
+  enum EState { quiescent, connecting, connected, disconnecting, disconnected };
+  EState m_state;
+
+  std::shared_ptr<ou::tf::ib::TWS> m_pProviderTWS;
+  std::shared_ptr<ou::tf::iqfeed::IQFeedProvider> m_pProviderIQFeed;
+
+  std::unique_ptr<ou::tf::db> m_pdb;
+
+  void Connected_IQFeed( int );
+  void Connected_TWS( int );
+  void Connected( int );
+
+  void Disconnected_IQFeed( int );
+  void Disconnected_TWS( int );
+  void Disconnected( int );
 };
