e06ba63f844381bbcbf41a2d04c3f23246de1a3b
Raymond Burkholder <raymond@burkholder.net>
2022-10-20 17:54:05 -0600

    Collector/ - skeleton for L1/L2 stream to disk

---
+++ Collector/main.cpp
@@ -0,0 +1,41 @@
+/************************************************************************
+ * Copyright(c) 2022, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    Main.cpp
+ * Author:  raymond@burkholder.net
+ * Project: Collector
+ * Created: October 20, 2022 17:48:11
+ */
+
+#include <string>
+
+int main( int argc, char* argv[] ) {
+
+  const static std::string sConfigFileName( "collector.cfg" );
+
+//  config::Choices choices;
+
+//  if ( Load( sConfigFileName, choices ) ) {
+//  }
+//  else {
+//    return EXIT_FAILURE;
+//  }
+
+  //Process process( choices, vSymbols );
+  //process.Wait();
+
+  return EXIT_SUCCESS;
+}
+
