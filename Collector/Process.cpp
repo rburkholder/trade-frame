11deff4647f109e8d3ef2707ccf431f69771b297
Raymond Burkholder <raymond@burkholder.net>
2022-10-30 19:47:02 -0600

    Collector/Process - build specific symbol

--- Collector/Process.cpp
+++ Collector/Process.cpp
@@ -19,27 +19,116 @@
  * Created: October 20, 2022 21:07:40
  */
 
+// convert/lookup name
+// start watch on l1, l2
+// write on regular intervals
+
+#include <TFTrading/BuildInstrument.h>
+
+#include <TFIQFeed/OptionChainQuery.h>
+
 #include "Process.hpp"
 
 Process::Process( const config::Choices& choices )
-: m_choices( choices )
+: m_choices( choices ), m_bDone( false )
 {
-  m_piqfeed = ou::tf::iqfeed::IQFeedProvider::Factory();
+  StartIQFeed();
+}
 
-  m_piqfeed->OnConnected.Add( MakeDelegate( this, &Process::HandleConnected ) );
-  m_piqfeed->Connect();
+Process::~Process() {
+  m_pOptionChainQuery.reset();
+  m_pBuildInstrumentIQFeed.reset();
+  m_pInstrumentUnderlying.reset();
+  m_piqfeed.reset();
 }
 
 // need control c handler to terminate, as this is an ongoing process
 
-void Process::HandleConnected( int ) {
-  // convert/lookup name
-  // start watch on l1, l2
-  // write on regular intervals
+void Process::StartIQFeed() {
+  m_piqfeed = ou::tf::iqfeed::IQFeedProvider::Factory();
+
+  m_piqfeed->OnConnected.Add( MakeDelegate( this, &Process::HandleIQFeedConnected ) );
+  m_piqfeed->Connect();
+}
+
+void Process::HandleIQFeedConnected( int ) {
+  StartChainQuery();
+}
+
+void Process::StartChainQuery() {
+  if ( m_pOptionChainQuery ) {}
+  else {
+    m_pOptionChainQuery = std::make_unique<ou::tf::iqfeed::OptionChainQuery>(
+      [this](){
+        HandleChainQueryConnected( 0 );
+      }
+    );
+    m_pOptionChainQuery->Connect(); // TODO: auto-connect instead?
+  }
+}
+
+void Process::HandleChainQueryConnected( int ) {
+  ConstructUnderlying();
+}
+
+void Process::ConstructUnderlying() {
+
+  assert( m_pOptionChainQuery );
+
+  m_pBuildInstrumentIQFeed = std::make_shared<ou::tf::BuildInstrument>( m_piqfeed );
+  assert( m_pBuildInstrumentIQFeed );
+
+  const std::string& sName( m_choices.m_sSymbolName );
+
+  if ( '#' == sName.back() ) { // assumes a general future, and need to find actual symbol
+    m_pBuildInstrumentIQFeed->Queue(
+      sName,
+      [this,sName]( pInstrument_t pInstrument ){
+
+        using OptionChainQuery = ou::tf::iqfeed::OptionChainQuery;
+
+        // determine the specific future which matches the continuous generic contract
+        boost::gregorian::date expiry( pInstrument->GetExpiry() );
+        std::string sBase( sName.substr( 0, sName.size() - 1 ) );
+        m_pOptionChainQuery->QueryFuturesChain(  // obtain a list of futures
+          sBase, "", "234" /* 2022, 2023, 2024 */ , "4" /* 4 months */,
+          [this,expiry]( const OptionChainQuery::FuturesList& list ){
+            for ( const OptionChainQuery::vSymbol_t::value_type sSymbol: list.vSymbol ) {
+              m_pBuildInstrumentIQFeed->Queue(
+                sSymbol,
+                [this,expiry]( pInstrument_t pInstrument ){
+                  if ( expiry == pInstrument->GetExpiry() ) {
+                    StartWatch( pInstrument );
+                  }
+                } );
+            }
+          }
+          );
+      }
+    );
+  }
+  else {
+    m_pBuildInstrumentIQFeed->Queue(
+      sName,
+      [this]( pInstrument_t pInstrument ){
+        StartWatch( pInstrument );
+      } );
+  }
+
+}
+
+void Process::StartWatch( pInstrument_t pInstrument ) {
+  std::cout << "future: " << pInstrument->GetInstrumentName() << std::endl;
+  m_bDone = true;
+  m_cvWait.notify_one();
 }
 
 void Process::Abort() {
 }
 
 void Process::Wait() {
+  std::unique_lock<std::mutex> lock( m_mutexWait );
+  m_cvWait.wait( lock, [this]{
+    return m_bDone;
+    } );
 }
--- Collector/Process.hpp
+++ Collector/Process.hpp
@@ -23,14 +23,27 @@
 
 #pragma once
 
+#include <thread>
+
 #include <TFIQFeed/Provider.h>
+#include <TFTrading/Instrument.h>
 
 #include "Config.hpp"
+
+namespace ou {
+namespace tf {
+  class BuildInstrument;
+namespace iqfeed {
+  class OptionChainQuery;
+} // namespace iqfeed
+} // namespace tf
+} // namespace ou
 
 class Process {
 public:
 
   Process( const config::Choices& );
+  ~Process();
 
   void Abort();
   void Wait();
@@ -38,10 +51,27 @@ public:
 protected:
 private:
 
+  bool m_bDone; // required for the nature of the condition variable
+  std::mutex m_mutexWait;
+  std::condition_variable m_cvWait;
+
   const config::Choices& m_choices;
+
+  using pInstrument_t = ou::tf::Instrument::pInstrument_t;
+  pInstrument_t m_pInstrumentUnderlying;
 
   using pIQFeed_t = ou::tf::iqfeed::IQFeedProvider::pProvider_t;
   pIQFeed_t m_piqfeed;
 
-  void HandleConnected( int );
+  std::shared_ptr<ou::tf::BuildInstrument> m_pBuildInstrumentIQFeed;
+
+  using pOptionChainQuery_t = std::shared_ptr<ou::tf::iqfeed::OptionChainQuery>;
+  pOptionChainQuery_t m_pOptionChainQuery; // need to disconnect
+
+  void StartIQFeed();
+  void HandleIQFeedConnected( int );
+  void StartChainQuery();
+  void HandleChainQueryConnected( int );
+  void ConstructUnderlying();
+  void StartWatch( pInstrument_t );
 };
