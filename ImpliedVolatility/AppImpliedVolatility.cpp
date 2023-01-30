d4f60615705f45c0258c2910e7ab825776e2f7ab
Raymond Burkholder <raymond@burkholder.net>
2023-01-30 00:15:13 -0700

    ImpliedVolatility/ - skeleton to track futures ATM IV

---
+++ ImpliedVolatility/AppImpliedVolatility.cpp
@@ -0,0 +1,193 @@
+/************************************************************************
+ * Copyright(c) 2023, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppImpliedVolatility.cpp
+ * Author:  raymond@burkholder.net
+ * Project: ImpliedVolatility
+ * Created: January 27, 2023  19:20:09
+ */
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <TFTrading/Watch.h>
+#include <TFTrading/BuildInstrument.h>
+
+#include <TFVuTrading/FrameMain.h>
+#include <TFVuTrading/PanelLogging.h>
+#include <TFVuTrading/WinChartView.h>
+#include <TFVuTrading/PanelProviderControlv2.hpp>
+
+#include <wx/sizer.h>
+
+#include "AppImpliedVolatility.hpp"
+
+namespace {
+  static const std::string sAppName( "Implied Volatility" );
+  static const std::string sVendorName( "One Unified Net Limited" );
+
+  static const std::string sDirectory( "iv" );
+  static const std::string sDbName( "ImpliedVolatility.db" );
+  static const std::string sStateFileName( "ImpliedVolatility.state" );
+  static const std::string sChoicesFilename( "ImpliedVolatility.cfg" );
+}
+
+// =============
+
+IMPLEMENT_APP(AppImpliedVolatility)
+
+bool AppImpliedVolatility::OnInit() {
+
+  wxApp::SetVendorName( sVendorName );
+  wxApp::SetAppDisplayName( sAppName );
+  wxApp::SetVendorDisplayName( "(c)2022 " + sVendorName );
+
+  wxApp::OnInit();
+
+  m_iqfeed = ou::tf::iqfeed::IQFeedProvider::Factory();
+  //m_iqfeed->SetThreadCount( m_choices.nThreads );
+
+  m_pFrameMain = new FrameMain( 0, wxID_ANY, sAppName );
+  wxWindowID idFrameMain = m_pFrameMain->GetId();
+
+  m_pFrameMain->SetSize( 800, 500 );
+  //m_pFrameMain->SetAutoLayout( true );
+
+  SetTopWindow( m_pFrameMain );
+
+  wxBoxSizer* sizerFrame;
+  wxBoxSizer* sizerUpper;
+  wxBoxSizer* sizerLower;
+
+  sizerFrame = new wxBoxSizer(wxVERTICAL);
+  m_pFrameMain->SetSizer(sizerFrame);
+
+  sizerUpper = new wxBoxSizer(wxHORIZONTAL);
+  sizerFrame->Add( sizerUpper, 0, wxEXPAND, 2);
+
+  m_pPanelProviderControl = new ou::tf::v2::PanelProviderControl( m_pFrameMain, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+  m_pPanelProviderControl->SetExtraStyle(wxWS_EX_VALIDATE_RECURSIVELY);
+  m_pPanelProviderControl->Show();
+
+  sizerUpper->Add( m_pPanelProviderControl, 0, wxALIGN_LEFT, 2);
+
+  m_pPanelProviderControl->Add(
+    m_iqfeed,
+    true, false, false, false,
+    [](){}, // fConnecting
+    [this](){ // fConnected
+      ConfirmProviders();
+    },
+    [](){}, // fDisconnecting
+    [this](){ // fDisconnected
+    }
+  );
+
+  FrameMain::vpItems_t vItems;
+  using mi = FrameMain::structMenuItem;  // vxWidgets takes ownership of the objects
+
+  vItems.clear();
+  vItems.push_back( new mi( "Close, Done", MakeDelegate( this, &AppImpliedVolatility::HandleMenuActionCloseAndDone ) ) );
+  vItems.push_back( new mi( "Save Values", MakeDelegate( this, &AppImpliedVolatility::HandleMenuActionSaveValues ) ) );
+  m_pFrameMain->AddDynamicMenu( "Actions", vItems );
+
+  m_pPanelLogging = new ou::tf::PanelLogging( m_pFrameMain, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER|wxTAB_TRAVERSAL );
+  m_pPanelLogging->SetExtraStyle(wxWS_EX_VALIDATE_RECURSIVELY);
+  sizerUpper->Add(m_pPanelLogging, 2, wxEXPAND, 2);
+
+  sizerLower = new wxBoxSizer(wxVERTICAL);
+  sizerFrame->Add(sizerLower, 1, wxEXPAND, 2);
+
+  m_pWinChartView = new ou::tf::WinChartView( m_splitterData, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER|wxTAB_TRAVERSAL );
+  m_pWinChartView->SetExtraStyle(wxWS_EX_VALIDATE_RECURSIVELY);
+
+  m_pFrameMain->Bind( wxEVT_CLOSE_WINDOW, &AppImpliedVolatility::OnClose, this );  // start close of windows and controls
+
+  CallAfter(
+    [this](){
+      LoadState();
+    }
+  );
+
+  m_pBuildInstrument = std::make_unique<ou::tf::BuildInstrument>( m_iqfeed );
+
+  m_pFrameMain->Show( true );
+
+  return 1;
+}
+
+void AppImpliedVolatility::HandleMenuActionCloseAndDone() {
+  std::cout << "Closing & Done" << std::endl;
+  CallAfter(
+    [this](){
+      //for ( mapStrategy_t::value_type& vt: m_mapStrategy ) {
+      //  vt.second->CloseAndDone();
+      //}
+    } );
+}
+
+void AppImpliedVolatility::HandleMenuActionSaveValues() {
+  std::cout << "Saving collected values to ";
+  CallAfter(
+    [this](){
+      m_nTSDataStreamSequence++; // sequence number on each save
+      std::string sPath(
+        "/app/" + sDirectory + "/" +
+        m_sTSDataStreamStarted + "-" +
+        boost::lexical_cast<std::string>( m_nTSDataStreamSequence ) );
+      std::cout << sPath << std::endl;
+      //for ( mapStrategy_t::value_type& vt: m_mapStrategy ) {
+      //  vt.second->SaveWatch( sPath );
+      //}
+      //if ( m_pFile ) { // performed at exit to ensure no duplication in file
+      //  m_pFile->Write();
+      //}
+      std::cout << "  ... Done " << std::endl;
+    }
+  );
+}
+
+void AppImpliedVolatility::ConfirmProviders() {
+}
+
+int AppImpliedVolatility::OnExit() {
+  return wxAppConsole::OnExit();
+}
+
+void AppImpliedVolatility::SaveState() {
+  std::cout << "Saving Config ..." << std::endl;
+  std::ofstream ofs( sStateFileName );
+  boost::archive::text_oarchive oa(ofs);
+  oa & *this;
+  std::cout << "  done." << std::endl;
+}
+
+void AppImpliedVolatility::LoadState() {
+  try {
+    std::cout << "Loading Config ..." << std::endl;
+    std::ifstream ifs( sStateFileName );
+    boost::archive::text_iarchive ia(ifs);
+    ia & *this;
+    std::cout << "  done." << std::endl;
+  }
+  catch(...) {
+    std::cout << "load exception" << std::endl;
+  }
+}
+
+void AppImpliedVolatility::OnClose( wxCloseEvent& event ) {
+  SaveState();
+  event.Skip();  // auto followed by Destroy();
+}
---
+++ ImpliedVolatility/AppImpliedVolatility.hpp
@@ -0,0 +1,110 @@
+/************************************************************************
+ * Copyright(c) 2023, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppImpliedVolatility.hpp
+ * Author:  raymond@burkholder.net
+ * Project: ImpliedVolatility
+ * Created: January 27, 2023  19:20:09
+ */
+
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/split_member.hpp>
+
+#include <wx/splitter.h>
+
+#include <TFIQFeed/Provider.h>
+
+#include <OUCharting/ChartDataView.h>
+#include <OUCharting/ChartEntryIndicator.h>
+
+#include <TFBitsNPieces/FrameWork02.hpp>
+
+#include <wx/app.h>
+
+class FrameMain;
+
+namespace ou {
+namespace tf {
+  class PanelLogging;
+  class WinChartView;
+  class BuildInstrument;
+namespace v2 {
+  class PanelProviderControl;
+}
+} // namespace tf
+} // namespace ou
+
+class AppImpliedVolatility:
+  public wxApp,
+  public ou::tf::FrameWork02<AppImpliedVolatility>
+{
+  friend ou::tf::FrameWork02<AppImpliedVolatility>;
+  friend class boost::serialization::access;
+public:
+protected:
+private:
+
+  using pProviderIQFeed_t = ou::tf::iqfeed::IQFeedProvider::pProvider_t;
+
+  FrameMain* m_pFrameMain;
+  ou::tf::PanelLogging* m_pPanelLogging;
+  ou::tf::WinChartView* m_pWinChartView;
+  ou::tf::v2::PanelProviderControl* m_pPanelProviderControl;
+
+  wxSplitterWindow* m_splitterData;
+
+  pProviderIQFeed_t m_iqfeed; // live - data
+
+  std::unique_ptr<ou::tf::BuildInstrument> m_pBuildInstrument;
+
+  //std::unique_ptr<ou::tf::db> m_pdb;
+
+  ou::ChartDataView m_dvChart; // the data
+
+  virtual bool OnInit();
+  virtual int OnExit();
+  void OnClose( wxCloseEvent& event );
+
+  void HandleMenuActionCloseAndDone();
+  void HandleMenuActionSaveValues();
+
+  void ConfirmProviders();
+
+  void SaveState();
+  void LoadState();
+
+  template<typename Archive>
+  void save( Archive& ar, const unsigned int version ) const {
+    ar & *m_pFrameMain;
+    ar & m_splitterData->GetSashPosition();
+  }
+
+  template<typename Archive>
+  void load( Archive& ar, const unsigned int version ) {
+    ar & *m_pFrameMain;
+    if ( 2 <= version ) {
+      int x;
+      ar & x;
+      m_splitterData->SetSashPosition( x );
+    }
+  }
+
+  BOOST_SERIALIZATION_SPLIT_MEMBER()
+
+};
+
+BOOST_CLASS_VERSION(AppImpliedVolatility, 1)
+
+DECLARE_APP(AppImpliedVolatility)
