4cb497961b8083602a746341f97bf12c761a8272
Raymond Burkholder <raymond@burkholder.net>
2023-02-16 12:29:18 -0700

    lib/TFTimeSeries/DatedDatum.cpp - change semantics of IsNonZero to "or"

--- lib/TFTimeSeries/DatedDatum.cpp
+++ lib/TFTimeSeries/DatedDatum.cpp
@@ -100,9 +100,10 @@ bool Quote::IsValid() const {
 };
 
 bool Quote::IsNonZero() const {
-  bool bOk( true );
-  bOk &= ( 0.0 != m_dblBid );
-  bOk &= ( 0.0 != m_dblAsk );
+  bool bOk( false );
+  bOk |= ( 0.0 < m_dblAsk );
+  bOk |= ( 0.0 < m_dblBid ); // can be zero for far out of the money options
+  // TODO: maybe make flag to accept for OTM options?
   //bOk &= ( ( 0 == m_nBidSize ) && ( 0.0 == m_dblBid ) ); // NOTE: some options are zero bid
   //bOk &= ( ( 0 != m_nAskSize ) && ( 0.0 != m_dblAsk ) );
   // TODO: what other tests?
