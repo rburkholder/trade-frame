3e677c868d7eedfd54e027cfff447bfea34918d0
Raymond Burkholder <raymond@burkholder.net>
2024-03-09 20:04:34 -0700

    CurrencyTrader - initial empty files

---
+++ CurrencyTrader/AppCurrencyTrader.hpp
@@ -0,0 +1,25 @@
+/************************************************************************
+ * Copyright(c) 2024, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppCurrencyTrader.hpp
+ * Author:  raymond@burkholder.net
+ * Project: CurrencyTrader
+ * Created: March 09, 2024 19:58:27
+ */
+
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/split_member.hpp>
+
+#include <wx/app.h>
---
+++ CurrencyTrader/Config.hpp
@@ -0,0 +1,21 @@
+/************************************************************************
+ * Copyright(c) 2024, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    Config.hpp
+ * Author:  raymond@burkholder.net
+ * Project: CurrencyTrader
+ * Created: March 09, 2024 19:58:27
+ */
+
