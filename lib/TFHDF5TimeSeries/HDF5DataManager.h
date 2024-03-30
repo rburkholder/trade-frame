0d745b91e08a2d017caacf0b5c2e29f0ea6c8367
Raymond Burkholder <raymond@burkholder.net>
2024-03-30 16:19:21 -0600

    lib/TFHDF5TimeSeries/HDF5DataManager - provide alternate hdf5 file, cleanup, function optimization

--- lib/TFHDF5TimeSeries/HDF5DataManager.h
+++ lib/TFHDF5TimeSeries/HDF5DataManager.h
@@ -14,20 +14,23 @@
 
 #pragma once
 
-// changed to lower case 2015/02/08
-#include <hdf5/H5Cpp.h>
+#include <functional>
 
-#include <boost/function.hpp>
+#include <hdf5/H5Cpp.h>
 
 namespace ou { // One Unified
 namespace tf { // TradeFrame
 
 class HDF5DataManager {
 public:
+
   enum enumFileOptionType{ RDWR, RO };
+
   HDF5DataManager( enumFileOptionType );
-  ~HDF5DataManager(void);
-  H5::H5File *GetH5File( void ) { return &m_H5File; };
+  HDF5DataManager( enumFileOptionType, const std::string& sName ); // alternate hdf5 file
+  ~HDF5DataManager();
+
+  H5::H5File *GetH5File() { return &m_H5File; };
   bool GroupExists( const std::string &sGroup );
   void AddGroup( const std::string &sGroupPath );  // last group needs trailing '/'
   void AddGroupForSymbol( const std::string &sSymbol );
@@ -35,12 +38,13 @@ public:
 //  static hsize_t H5ChunkSize( void ) { return 1024; };  // # elements to be shuffled/compressed in one block,  was 64
 //  static hsize_t H5ChunkSize( void ) { return 32; };  // # elements to be shuffled/compressed in one block,  was 64
   static void DailyBarPath( const std::string &sSymbol, std::string &sPath );
-  void Flush( void );
+  void Flush();
 
-  typedef boost::function<void (const std::string& )> callbackIteratePath_t;
-  void IteratePathParts( const std::string& sPath, callbackIteratePath_t object );
+  using fCallbackIteratorPath_t = std::function<void(const std::string&)>;
+  void IteratePathParts( const std::string& sPath, fCallbackIteratorPath_t&& );
+
 protected:
-  static const char m_H5FileName[];
+  static const char c_H5FileName[];
 //  static unsigned int m_RefCount;
 //  static H5::H5File m_H5File;
   H5::H5File m_H5File;
@@ -49,26 +53,3 @@ private:
 
 } // namespace tf
 } // namespace ou
-
-
-/*
-If you are only writing to very small datasets, then the default 
-chunk cache size (1 MB) is most likely large enough, since this limit 
-is applied to each dataset individually.  However, if you are regularly 
-rewriting/reading the same portions of the dataset, and it can grow beyond 
-1 MB then you may see a benefit  from increasing the cache size.  
-
-Depending on your chunk size, you may also want to increase the number of 
-elements in the chunk cache from the default 521 (make sure it stays a prime number).  
-Be careful about having too many datasets open at once though, as the limit is 1 MB 
-for each dataset.  So if you have several million datasets open  you potentially 
-have several million megabytes of cache.
-
-The chunk size should align as closely as possible to your typcial selection for 
-writing (or reading).  This minimizes the amount of costly scattering as well as 
-wasted space in the cache.  However you should not set it too small, in order to avoid excessive overhead.
-
-Thanks,
--Neil Fortner
-Neil Fortner [nfortne2@hdfgroup.org]
-*/
