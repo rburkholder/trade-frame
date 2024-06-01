d985ddb34a9c85b9e80bc0785b0fae95709e10ac
Raymond Burkholder <raymond@burkholder.net>
2024-06-01 16:28:50 -0600

    CurrencyTrader/Common.hpp - add in missing header

---
+++ CurrencyTrader/Common.hpp
@@ -0,0 +1,25 @@
+/************************************************************************
+ * Copyright(c) 2024, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    Common.hpp
+ * Author:  raymond@burkholder.net
+ * Project: CurrencyTrader
+ * Created: June 1, 2024 14:06:36
+ */
+
+#pragma once
+
+// is base currency the first or second part of a pair, eg, usd.cad
+enum class EBase { Unknown, First, Second };
