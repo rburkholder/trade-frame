a505bf284e7df6d8b818a18aacff3052ea8a1c07
Raymond Burkholder <raymond@burkholder.net>
2024-11-10 12:39:47 -0700

    MarketTrader - new attempt at kitchen sink

---
+++ MarketTrader/AppMarketTrader.cpp
@@ -0,0 +1,33 @@
+/************************************************************************
+ * Copyright(c) 2024, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppMarketTrader.cpp
+ * Author:  raymond@burkholder.net
+ * Project: Trader
+ * Created: 2024/11/10 12:18:19
+ */
+
+// TODO: convert to wswidgets
+// TODO: add telegram, lua,
+// TODO: live charts, charts from history
+// TODO: futures, equities, option combos
+
+ #include "AppMarketTrader.hpp"
+
+int main( int argc, char* argv[] ) {
+  //return EXIT_SUCCESS;
+  AppTrader trader;
+  return 0;
+}
---
+++ MarketTrader/AppMarketTrader.hpp
@@ -0,0 +1,28 @@
+/************************************************************************
+ * Copyright(c) 2024, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppMarketTrader.hpp
+ * Author:  raymond@burkholder.net
+ * Project: Trader
+ * Created: 2024/11/10 12:18:19
+ */
+
+#pragma once
+
+class AppTrader {
+public:
+protected:
+private:
+};
