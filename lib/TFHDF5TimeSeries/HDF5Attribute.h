39ee928dce40125fa07d5539ad3454b9ca6f0069
Raymond Burkholder <raymond@burkholder.net>
2025-04-05 20:03:32 -0600

    lib/TFHDF5TimeSeries/HDF5Attribute.h - remove some 'void'

--- lib/TFHDF5TimeSeries/HDF5Attribute.h
+++ lib/TFHDF5TimeSeries/HDF5Attribute.h
@@ -18,9 +18,10 @@
 
 #include <boost/cstdint.hpp>
 
-#include "HDF5DataManager.h"
 #include <TFTrading/TradingEnumerations.h>
 #include <TFTrading/KeyTypes.h>
+
+#include "HDF5DataManager.h"
 
 namespace ou { // One Unified
 namespace tf { // TradeFrame
@@ -35,18 +36,18 @@ public:
     unsigned short nMonth;
     unsigned short nDay;
     OptionSide::EOptionSide eSide;
-    structOption( void ) : dblStrike( 0 ), nYear( 0 ), nMonth( 0 ), nDay( 0 ), eSide( OptionSide::Unknown ) {};
+    structOption() : dblStrike( 0 ), nYear( 0 ), nMonth( 0 ), nDay( 0 ), eSide( OptionSide::Unknown ) {};
     structOption( double dblStrike_, unsigned short nYear_, unsigned short nMonth_, unsigned short nDay_, OptionSide::EOptionSide eSide_ )
-      : dblStrike( dblStrike_ ), nYear( nYear_ ), nMonth( nMonth_ ), nDay( nDay_), eSide( eSide_ ) {};
+    : dblStrike( dblStrike_ ), nYear( nYear_ ), nMonth( nMonth_ ), nDay( nDay_), eSide( eSide_ ) {};
   };
 
   struct structFuture {
     unsigned short nYear;
     unsigned short nMonth;
     unsigned short nDay;
-    structFuture( void ) :nYear( 0 ), nMonth( 0 ), nDay( 0 ) {};
+    structFuture() :nYear( 0 ), nMonth( 0 ), nDay( 0 ) {};
     structFuture( unsigned short nYear_, unsigned short nMonth_, unsigned short nDay_ )
-      : nYear( nYear_ ), nMonth( nMonth_ ), nDay( nDay_) {};
+    : nYear( nYear_ ), nMonth( nMonth_ ), nDay( nDay_) {};
   };
 
   HDF5Attributes( HDF5DataManager& dm );
@@ -54,16 +55,16 @@ public:
   HDF5Attributes( HDF5DataManager& dm, const std::string& sPath, InstrumentType::EInstrumentType );
   HDF5Attributes( HDF5DataManager& dm, const std::string& sPath, const structOption& );
   HDF5Attributes( HDF5DataManager& dm, const std::string& sPath, const structFuture& );
-  ~HDF5Attributes(void);
+  ~HDF5Attributes();
 
   void SetSignature( boost::uint64_t ); // left to right reading: 9=datetime, 8=char, 1=double, 2=16 3=32, 4=64
   boost::uint64_t GetSignature();
 
   void SetInstrumentType( InstrumentType::EInstrumentType );
-  InstrumentType::EInstrumentType GetInstrumentType( void );
+  InstrumentType::EInstrumentType GetInstrumentType();
 
   void SetProviderType( keytypes::eidProvider_t );
-  keytypes::eidProvider_t GetProviderType( void );
+  keytypes::eidProvider_t GetProviderType();
 
   void SetOptionAttributes( const structOption& );
   void GetOptionAttributes( structOption* );
@@ -72,14 +73,14 @@ public:
   void GetFutureAttributes( structFuture* );
 
   void SetSignificantDigits( unsigned char );
-  unsigned char GetSignificantDigits( void );
+  unsigned char GetSignificantDigits();
 
   void SetMultiplier( unsigned short );
-  unsigned short GetMultiplier( void );
+  unsigned short GetMultiplier();
 
 protected:
   void OpenDataSet( const std::string& sPath );
-  void CloseDataSet( void );
+  void CloseDataSet();
 private:
   HDF5DataManager& m_dm;
   H5::DataSet *m_pDataSet;
