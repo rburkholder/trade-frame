6bcef7d07f73e434fcd252194fa8135fedfb1beb
Raymond Burkholder <raymond@burkholder.net>
2025-04-06 11:08:39 -0600

    x64/AppWebTrader/start.sh -> x64/AppWebTrader/init.sh - ln to resources instead

--- x64/AppWebTrader/init.sh
+++ x64/AppWebTrader/init.sh
@@ -2,4 +2,5 @@
 ../../../build/WebTrader/AppWebTrader "--docroot=web;/favicon.ico,/resources,/style,/image" --http-address=0.0.0.0 --http-port=8091 --config=etc/wt_config.xml
 # required resources:
 # cp -r ../libs-build/wt/resources x64/debug/AppWebTrader/web/
+# ln -s  /usr/local/share/Wt/resources web/resources
 
