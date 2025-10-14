7851aaaa0b339daf8d1ffe6cb07d2eb1ecbb0f3e
Raymond Burkholder <raymond@burkholder.net>
2025-10-13 23:44:11 -0600

    x64/debug/MarketTrader/lua/watch.lua - test price comparisons

--- x64/debug/MarketTrader/lua/watch.lua
+++ x64/debug/MarketTrader/lua/watch.lua
@@ -5,19 +5,29 @@
 description = 'watch a symbol and react'
 
 --local symbol = 'SPY'
-local symbol = '@ES#'
+--local symbol = '@ES#'
+local symbol = 'QGCZ25'
 local instrument = nil
 
 handle_trade = function( price, volume ) -- called from c++
-  print( symbol .. ': ' .. volume .. '@' .. price )
+  local hi = 4189
+  local lo = 4187
+  if hi < price then
+    --telegram_message( 'symbol ' .. symbol .. 'hi(' .. hi .. ') @ ' .. price )
+  end
+  if min > price then
+    --telegram_message( 'symbol ' .. symbol .. 'lo(' .. lo .. ') @ ' .. price )
+  end
+  -- print( symbol .. ': ' .. volume .. '@' .. price )
 end
 
 attach = function()
   instrument = tie.new();
   instrument:watch( symbol )
-  --telegram_message( 'watching ' .. symbol )
+  telegram_message( 'watching ' .. symbol )
 end
 
 detach = function()
+  telegram_message( 'un-watch ' .. symbol )
   instrument = nil;
 end
