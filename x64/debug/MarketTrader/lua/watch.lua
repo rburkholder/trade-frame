bd957e3eef7b8c719198a59730352086248d763e
Raymond Burkholder <raymond@burkholder.net>
2025-10-19 10:06:25 -0600

    x64/debug/MarketTrader/lua/watch.lua - notes for potential enhancements

--- x64/debug/MarketTrader/lua/watch.lua
+++ x64/debug/MarketTrader/lua/watch.lua
@@ -4,14 +4,31 @@
 
 description = 'watch a symbol and react'
 
+-- commands:
+--   start watch on symbol (create a new instance here)
+--   stop watch
+
+--   show triggers
+--   kill trigger hi
+--   kill trigger lo
+--   set trigger hi
+--   set trigger lo
+--   current quote
+
+-- current news items
+
 --local symbol = 'SPY'
 --local symbol = '@ES#'
 local symbol = 'QGCZ25'
 local instrument = nil
 
+local hi_trigger = 4150
+local hi_triggered = false
+
+local lo_trigger = 4100
+local lo_triggered = false
+
 handle_trade = function( price, volume ) -- called from c++
-  local hi = 4189
-  local lo = 4187
   if hi < price then
     --telegram_message( 'symbol ' .. symbol .. 'hi(' .. hi .. ') @ ' .. price )
   end
@@ -24,10 +41,10 @@ end
 attach = function()
   instrument = tie.new();
   instrument:watch( symbol )
-  telegram_message( 'watching ' .. symbol )
+  telegram_message( symbol .. ' - watch start' )
 end
 
 detach = function()
-  telegram_message( 'un-watch ' .. symbol )
+  telegram_message( symbol .. ' - watch stop'  )
   instrument = nil;
 end
